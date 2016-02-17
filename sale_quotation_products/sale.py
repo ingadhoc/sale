4a73d6033138eaa56be110b80ac8a33bbfc9a486
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-02-17 18:16:24 -0300

    FIX sale uqotations productos for multic

--- sale_quotation_products/sale.py
+++ sale_quotation_products/sale.py
@@ -24,6 +24,8 @@ class sale_order(models.Model):
             action_read = actions.read()[0]
             context = literal_eval(action_read['context'])
             context['pricelist'] = self.pricelist_id.display_name
+            # we send company in context so it filters taxes
+            context['company_id'] = self.company_id.id
             action_read['context'] = context
             # this search view removes pricelist
             action_read.pop("search_view", None)
