da4262cdf704d2e634b569cd3230968607b1c73a
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-08-26 16:57:48 -0300

    INIT re init v9 from 8.0

---
+++ l10n_ar_sale/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from . import company
+from . import sale
+# from . import res_config
+from . import report
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
