cd02372ab510eaa445f042e50380253eb494dccd
Nicolas Mac Rouillon <nmr@adhoc.com.ar>
2016-10-17 16:28:57 -0300

    FIX

--- sale_order_type_automation/tests/test_sale_order_type_automation.py
+++ sale_order_type_automation/tests/test_sale_order_type_automation.py
@@ -11,14 +11,26 @@ class TestSaleOrderTypeAutomation(common.TransactionCase):
     def setUp(self):
         super(TestSaleOrderTypeAutomation, self).setUp()
         self.sale_order_model = self.env['sale.order']
-        self.partner = self.env.ref('base.res_partner_1')
         self.sale_order_type = self.env.ref('sale_order_type.normal_sale_type')
+        self.sequence = self.env['ir.sequence'].create({
+            'name': 'Test Sales Order 2',
+            'code': 'sale.order',
+            'prefix': 'TSO2',
+            'padding': 3,
+            'company_id': self.sale_order_type.warehouse_id.company_id.id,
+        })
+        self.partner = self.env.ref('base.res_partner_1')
         self.product = self.env.ref('product.product_product_4')
-        self.sale_order_type.validate_automatically_picking = True
-        self.sale_order_type.validate_automatically_invoice = True
-        self.sale_order_type.validate_automatically_payment = True
-        self.sale_order_type.payment_journal_id = self.env[
-            'account.journal'].search([('type', '=', 'sale')], limit=1)
+        self.payment_journal = self.env[
+            'account.journal'].search(
+                [('type', '=', 'cash')], limit=1)
+        self.sale_type = self.sale_order_type.write({
+            'validate_automatically_picking': True,
+            'validate_automatically_invoice': True,
+            'validate_automatically_payment': True,
+            'payment_journal_id': self.payment_journal.id,
+            'sequence_id': self.sequence.id,
+        })
 
     def test_sale_order_confirm(self):
         sale_line_dict = {
