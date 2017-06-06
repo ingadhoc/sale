46f0ba5a029da92b715b8d19813cbc7ace140848
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-06-06 11:51:47 -0300

    FIX sale usability stock delivery status on sol pop form

--- sale_usability_stock/__openerp__.py
+++ sale_usability_stock/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Sale Usability interaction with Stock',
-    'version': '9.0.1.0.0',
+    'version': '9.0.1.1.0',
     'category': 'Sales',
     'sequence': 14,
     'summary': '',
