7bcb48271177103c38fee22f36ac4cb3f3b2557e
nicomacr <nmr@adhoc.com.ar>
2017-06-15 13:47:34 -0300

    FIx _prepare_order_line_procurement

--- sale_procurement_date_confirm/models/sale.py
+++ sale_procurement_date_confirm/models/sale.py
@@ -31,9 +31,18 @@ class SaleOrderLine(models.Model):
     def _prepare_order_line_procurement(self, group_id=False):
         res = super(SaleOrderLine, self)._prepare_order_line_procurement(
             group_id=group_id)
-        if self.order_id.date_confirm:
+
+        # si existe el campo 'requested_date' entonces sale_order_dates
+        # esta instalado, si viene seteado, dejamos que ese modulo
+        # setee la fecha planeada
+        order = self.order_id
+        if 'requested_date' in order._fields and order.requested_date:
+            return res
+        elif self.order_id.date_confirm:
             res['date_planned'] = datetime.strptime(
                 self.order_id.date_confirm, "%Y-%m-%d")\
                 + timedelta(days=self.customer_lead or 0.0) - \
                 timedelta(days=self.order_id.company_id.security_lead)
-        return res
+            return res
+        else:
+            return res
