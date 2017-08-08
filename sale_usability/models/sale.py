90fdaa360fcbd59ed6bba63207c87c2fcb2daef2
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-08-08 20:16:44 -0300

    IMP manually set invoiced

--- sale_usability/models/sale.py
+++ sale_usability/models/sale.py
@@ -3,12 +3,18 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from openerp import models, api, _
+from openerp import models, api, fields, _
 from openerp.exceptions import UserError
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+
+    manually_set_invoiced = fields.Boolean(
+        string='Manually Set Invoiced?',
+        help='If you set this field to True, then all lines invoiceable lines'
+        'will be set to invoiced?',
+    )
 
     @api.multi
     def action_cancel(self):
@@ -25,11 +31,20 @@ class SaleOrder(models.Model):
         self.write({'state': 'sale'})
 
     @api.multi
-    def button_set_invoiced(self):
+    @api.constrains('manually_set_invoiced')
+    def check_manually_set_invoiced(self):
         if not self.user_has_groups('base.group_system'):
             group = self.env.ref('base.group_system').sudo()
             raise UserError(_(
                 'Only users with "%s / %s" can Set Invoiced manually') % (
                 group.category_id.name, group.name))
-        self.order_line.write({'qty_to_invoice': 0.0})
-        self.message_post(body='Manually setted as invoiced')
+
+    # @api.multi
+    # def button_set_invoiced(self):
+    #     if not self.user_has_groups('base.group_system'):
+    #         group = self.env.ref('base.group_system').sudo()
+    #         raise UserError(_(
+    #             'Only users with "%s / %s" can Set Invoiced manually') % (
+    #             group.category_id.name, group.name))
+    #     self.order_line.write({'qty_to_invoice': 0.0})
+    #     self.message_post(body='Manually setted as invoiced')
--- sale_usability/models/sale_order_line.py
+++ sale_usability/models/sale_order_line.py
@@ -3,7 +3,7 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class SaleOrderLine(models.Model):
@@ -13,3 +13,19 @@ class SaleOrderLine(models.Model):
     order_id = fields.Many2one(
         context={'show_sale': True}
     )
+
+    @api.depends('order_id.manually_set_invoiced')
+    def _compute_invoice_status(self):
+        """
+        Sobreescribimos directamente el invoice status y no el qty_to_invoice
+        ya que no nos importa tipo de producto y lo hace mas facil.
+        Ademas no molesta dependencias con otros modulos que ya sobreescribian
+        _get_to_invoice_qty
+        """
+        super(SaleOrderLine, self)._compute_invoice_status()
+        for line in self:
+            # solo seteamos facturado si en sale o done
+            if line.order_id.state not in ['sale', 'done']:
+                continue
+            if line.order_id.manually_set_invoiced:
+                line.invoice_status = 'invoiced'
