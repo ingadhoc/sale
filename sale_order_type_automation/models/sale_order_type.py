3d55a4b1b6e99a52fb0f8e182e137497013516fb
nicomacr <nmr@adhoc.com.ar>
2017-08-23 13:14:09 -0300

    FIX sale_order_type_automatation with force company

--- sale_order_type_automation/models/sale_order_type.py
+++ sale_order_type_automation/models/sale_order_type.py
@@ -21,6 +21,10 @@ class SaleOrderTypology(models.Model):
         'created automatically or manually. If no journal is set here, '
         'default journal will be used'
     )
+    invoice_company_id = fields.Many2one(
+        related='journal_id.company_id',
+        readonly=True
+    )
     invoicing_atomation = fields.Selection([
         ('none', 'None'),
         ('create_invoice', 'Create Invoice'),
@@ -64,7 +68,8 @@ class SaleOrderTypology(models.Model):
         'account.journal',
         'Payment Journal',
         domain="[('type','in', ['cash', 'bank']), "
-        "('company_id', '=', company_id), "
+        "('company_id', '=', invoice_company_id), "
+        # "('company_id', '=', company_id), "
         "('inbound_payment_method_ids.code', '=', 'manual')]",
         help='Jouranl used only with payment_automation. As manual payment '
         'method is used, only journals with manual method are shown.'
@@ -90,21 +95,32 @@ class SaleOrderTypology(models.Model):
         'payment_journal_id',
         'sequence_id')
     def validate_company_id(self):
-        text = _(
-            'The Journal "%s" company must be the same than sale order type')
-        for sale_type in self:
-            if sale_type.journal_id and sale_type.journal_id.company_id\
-                    != sale_type.company_id:
-                raise ValidationError(text % sale_type.journal_id.name)
-            if sale_type.payment_journal_id and \
-                    sale_type.payment_journal_id.company_id != \
-                    sale_type.company_id:
-                raise ValidationError(text % sale_type.payment_journal_id.name)
+        # text = _(
+        #     'The Journal "%s" company must be the same than sale order type')
+        for rec in self:
+            invoice_company = rec.invoice_company_id
+            payment_company = rec.payment_journal_id.company_id
+            if invoice_company and payment_company and \
+                    invoice_company != payment_company:
+                raise ValidationError(_(
+                    'Invoice Journal and Payment Journal must be of the same '
+                    'company'))
+            # TODO ver si borramos, por ahora lo desactivamos porque queremos
+            # permitir que se facture en cias hijas del almacen del sale type
+            # if rec.journal_id and rec.journal_id.company_id\
+            #         != rec.company_id:
+            #     raise ValidationError(text % rec.journal_id.name)
+            # if rec.payment_journal_id and \
+            #         rec.payment_journal_id.company_id != \
+            #         rec.company_id:
+            #     raise ValidationError(
+            #         text % rec.payment_journal_id.name)
+
             # la cia es opcional en la secuencia, solo chequeamos si esta
             # seteada
             # TODO this should go in a pr to OCA sot module
-            if sale_type.sequence_id.company_id and (
-                    sale_type.sequence_id.company_id != sale_type.company_id):
+            if rec.sequence_id.company_id and (
+                    rec.sequence_id.company_id != rec.company_id):
                 raise ValidationError(_(
                     'The Sequence "%s" company must be the same than'
-                    ' sale order type') % sale_type.sequence_id.name)
+                    ' sale order type') % rec.sequence_id.name)
