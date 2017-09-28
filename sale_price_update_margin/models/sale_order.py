402120d31beeb76353e49ce870149ada6a766f32
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2017-09-28 16:27:48 -0300

    FIX tests

--- sale_price_update_margin/models/sale_order.py
+++ sale_price_update_margin/models/sale_order.py
@@ -1,13 +1,17 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
 from openerp import models, api
 
 
-class saleOrder(models.Model):
+class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     @api.multi
     def update_prices(self):
-        super(saleOrder, self).update_prices()
+        super(SaleOrder, self).update_prices()
         for line in self.order_line:
             purchase_price = line._compute_margin(
                 self, line.product_id, line.product_uom)
