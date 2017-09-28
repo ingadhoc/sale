5049cf7d3732493c574a54ff06b74626da845c09
nicomacr <nmr@adhoc.com.ar>
2017-09-28 10:38:13 -0300

    FIX

--- sale_price_update_margin/models/sale_order.py
+++ sale_price_update_margin/models/sale_order.py
@@ -2,12 +2,12 @@
 from openerp import models, api
 
 
-class saleOrder(models.Model):
+class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     @api.multi
     def update_prices(self):
-        super(saleOrder, self).update_prices()
+        super(SaleOrder, self).update_prices()
         for line in self.order_line:
             purchase_price = line._compute_margin(
                 self, line.product_id, line.product_uom)
