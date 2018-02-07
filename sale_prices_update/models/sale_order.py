7592187c402e3035e6dcc3be81b1e0b6dc2d8cc4
nicomacr <nmr@adhoc.com.ar>
2018-02-07 09:52:07 -0300

    9.0 FIX sale_prices_update for compatibility with product_pack module

--- sale_prices_update/models/sale_order.py
+++ sale_prices_update/models/sale_order.py
@@ -7,13 +7,20 @@ class sale_order(models.Model):
 
     @api.multi
     def update_prices(self):
+        # for compatibility with product_pack module
+        pack_installed = 'pack_parent_line_id' in self.order_line._fields
         for line in self.order_line:
-            price = self.pricelist_id.with_context(
-                uom=line.product_uom.id,
-                date=self.date_order).price_get(
-                line.product_id.id,
-                line.product_uom_qty or 1.0,
-                self.partner_id.id
-            )[self.pricelist_id.id]
+            if pack_installed and line.pack_parent_line_id.\
+                product_id.pack_price_type in [
+                    'fixed_price', 'totalice_price']:
+                price = 0.0
+            else:
+                price = self.pricelist_id.with_context(
+                    uom=line.product_uom.id,
+                    date=self.date_order).price_get(
+                    line.product_id.id,
+                    line.product_uom_qty or 1.0,
+                    self.partner_id.id
+                )[self.pricelist_id.id]
             line.write({'price_unit': price})
         return True
