1c3cebb352b0e8911ab650ee27c277d712748aca
Katherine Zaoral <kz@adhoc.com.ar>
2018-05-15 12:51:10 -0300

    [ADD] sale_timesheet_ux: New module to create project from template
    A new field is added to the product type service: Create from project. This is
    a project related field that will let us know that instead of create a new
    project will copy from an existing project.

---
+++ sale_timesheet_ux/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Timesheet UX",
+    "summary": "",
+    "version": "11.0.1.0.0",
+    "website": "www.adhoc.com.ar",
+    "license": "AGPL-3",
+    "author": "ADHOC SA",
+    "category": "sale",
+    "depends": [
+        "sale_timesheet",
+    ],
+    "data": [
+        "views/product_template_views.xml",
+    ],
+    "demo": [
+    ],
+    "application": False,
+    "installable": True,
+}
