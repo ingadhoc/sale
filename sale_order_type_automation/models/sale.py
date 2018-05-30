08b84b50aac37152cc901b3d1a60243ec853be99
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2018-05-30 19:47:16 -0300

    [MERGE] port-forward from 9.0 * [ADD] to sale advance payment invoice compatibility with sale order type (#109)

--- sale_order_type_automation/models/sale.py
+++ sale_order_type_automation/models/sale.py
@@ -94,3 +94,18 @@ class SaleOrder(models.Model):
         if self.type_id.payment_atomation and self.type_id.payment_journal_id:
             res['pay_now_journal_id'] = self.type_id.payment_journal_id.id
         return res
+
+
+class SaleAdvancePaymentInv(models.TransientModel):
+    _inherit = "sale.advance.payment.inv"
+
+    @api.multi
+    def _create_invoice(self, order, so_line, amount):
+        if order.type_id.journal_id:
+            self = self.with_context(
+                default_sale_type_id=order.type_id.id,
+                default_journal_id=order.type_id.journal_id.id,
+            )
+            invoice = super(SaleAdvancePaymentInv, self)._create_invoice(
+                order=order, so_line=so_line, amount=amount)
+        return invoice
