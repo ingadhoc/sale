42f97b8530e20b0a1da202098ad4315527df8c75
Katherine Zaoral <zaoral@users.noreply.github.com>
2018-07-13 13:02:36 -0300

    [REL] sale_contract_editable: Deprecated module version 11.0 (#132)

--- sale_contract_editable/sale.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-from odoo import models, fields
-
-
-class sale_order(models.Model):
-    _inherit = "sale.order"
-
-    project_id = fields.Many2one(states={}, readonly=False)
