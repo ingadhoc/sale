5e0929a35bfa6700f06947c5ef67f34b3c213f95
ValentinoDefelice <vd@adhoc.com.ar>
2018-07-25 15:36:12 -0300

    [REL] sale_require_ref to V11. (#138)
    [FIX] Update models and files.
    [FIX] Update README.

--- sale_require_ref/__init__.py
+++ sale_require_ref/__init__.py
@@ -2,4 +2,3 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
