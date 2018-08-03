832657baf4c7056af789f3f8e6134778f43c60af
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2018-08-03 14:31:40 -0300

    [REL] Migration: sale_exception_partner_state to 11.0 (#150)
    * [REL] Migration: sale_exception_partner_state to 11.0
    
    * [FIX] Update Readme and Manifest
    * [FIX] Generate the sale config to the parameters in the company
    * [FIX] change models files name accodiding guidelines
    * [FIX] Change the .xml files name according guidelines
    
    * [ADD] oca depends

---
+++ sale_exception_partner_state/wizards/res_config_settings.py
@@ -0,0 +1,17 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+
+    _inherit = 'res.config.settings'
+
+    restrict_sales = fields.Selection(
+        related='company_id.restrict_sales'
+    )
+    restrict_sales_amount = fields.Float(
+        related='company_id.restrict_sales_amount'
+    )
