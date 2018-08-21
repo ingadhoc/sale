0225e20efae96b39337f2651b5f379e1b92dbd20
nicomacr <nmr@adhoc.com.ar>
2018-08-21 10:16:49 -0300

    [IMP] Fix cost computation on sale order lines

---
+++ sale_margin_ux/models/sale_order_line.py
@@ -0,0 +1,27 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    def _compute_margin(self, order_id, product_id, product_uom_id):
+        """ Overwrite this method and use frm_cur from product currency instead
+        of user company currency
+        This should be fixed on odoo
+        """
+        frm_cur = product_id.currency_id
+        to_cur = order_id.pricelist_id.currency_id
+        purchase_price = product_id.standard_price
+        if product_uom_id != product_id.uom_id:
+            purchase_price = self.env['product.uom']._compute_price(
+                product_id.uom_id.id, purchase_price,
+                to_uom_id=product_uom_id.id)
+        ctx = self.env.context.copy()
+        ctx['date'] = order_id.date_order
+        price = frm_cur.with_context(ctx).compute(
+            purchase_price, to_cur, round=False)
+        return price
