4d1670a13b441722f8bc3687e195220f240410f7
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2018-08-22 13:24:40 -0300

    [ADD] sale_margin_ux back-port from v11 (#158)

---
+++ sale_margin_ux/__init__.py
@@ -0,0 +1,5 @@
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from . import models
