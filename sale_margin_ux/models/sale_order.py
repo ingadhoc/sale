b5dc326484a1ce87df0f39c411f72bfb95dd944f
nicomacr <nmr@adhoc.com.ar>
2018-08-22 13:09:22 -0300

    [ADD] sale_margin_ux back-port from v11

---
+++ sale_margin_ux/models/sale_order.py
@@ -0,0 +1,18 @@
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from openerp import models, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def update_prices(self):
+        super(SaleOrder, self).update_prices()
+        for line in self.order_line:
+            purchase_price = line._compute_margin(
+                self, line.product_id, line.product_uom)
+            line.update({'purchase_price': purchase_price})
+        return True
