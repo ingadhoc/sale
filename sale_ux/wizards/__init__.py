c78935d18943ea87bcee82dde5d52fc604c5d2b2
Valentino <vd@adhoc.com.ar>
2018-11-27 12:54:22 -0300

    [ADD] Add field to sale advance payment calculated & inverse function to calculate amount total. [FIX] Update manifest.

--- sale_ux/wizards/__init__.py
+++ sale_ux/wizards/__init__.py
@@ -4,3 +4,4 @@
 ##############################################################################
 from . import res_config_settings
 from . import sale_global_discount_wizard
+from . import sale_advance_payment_inv
