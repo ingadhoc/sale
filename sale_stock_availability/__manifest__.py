5bea246c91540074ff32f72a650711e4b9075e46
nicomacr <nmr@adhoc.com.ar>
2018-11-30 15:00:59 -0300

    [IMP] hide the column virtual avaliable in so if the state of then are diferent that draft

--- sale_stock_availability/__manifest__.py
+++ sale_stock_availability/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Stock availability in sales order line',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.1.0',
     'category': 'Tools',
     'author': 'Moldeo Interactive, ADHOC SA',
     'website': 'http://business.moldeo.coop http://adhoc.com.ar/',
