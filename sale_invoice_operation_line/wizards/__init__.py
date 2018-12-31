744418f37863e05b8b70c17cfa87001be6b6d3c3
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-12-31 11:15:28 -0300

    [REM] sale_invoice_operation_line

--- sale_invoice_operation_line/wizards/__init__.py
+++
@@ -1,6 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import sale_order_line_operation_wizard
-from . import account_invoice_line_operation_wizard
