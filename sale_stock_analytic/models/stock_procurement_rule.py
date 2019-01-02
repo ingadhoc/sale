58a45d0ffcbccdc2d9718587993ca9f7dff3eb46
Valentino <vd@adhoc.com.ar>
2019-01-02 12:54:24 -0300

    [ADD] sale_stock_analytic

---
+++ sale_stock_analytic/models/stock_procurement_rule.py
@@ -0,0 +1,24 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import models
+
+
+class ProcurementRule(models.Model):
+    _inherit = 'procurement.rule'
+
+    def _get_stock_move_values(
+            self, product_id, product_qty, product_uom, location_id, name,
+            origin, values, group_id):
+        result = super(
+            ProcurementRule, self)._get_stock_move_values(
+                product_id, product_qty, product_uom, location_id, name,
+                origin, values, group_id)
+        sale_line_id = values.get('sale_line_id', False)
+        if sale_line_id:
+            result['analytic_tag_ids'] = [
+                (6, 0, self.env[
+                    'sale.order.line'].browse(
+                        sale_line_id).analytic_tag_ids.ids)]
+        return result
