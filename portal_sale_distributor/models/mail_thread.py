91fce92641dc2953d2ca3a0feadcf118f5d7c315
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-01-10 12:20:16 -0300

    [IMP] portal distributor

---
+++ portal_sale_distributor/models/mail_thread.py
@@ -0,0 +1,18 @@
+from odoo import models, api
+
+class MailThread(models.AbstractModel):
+    _inherit = 'mail.thread'
+
+    @api.multi
+    def _message_auto_subscribe_notify(self, partner_ids):
+        """ Cuando usuario portal crea OV se manda un mensjae de suscripcion
+        al comercial, esto terminahaciendo que se arroje error si se tiene
+        instalado mass_mailing y las estadisticas por permiso de acceso
+        a estadisticas. De esta forma lo arreglamos para que no haga falta
+        modulo puente
+        """
+        if self._name == 'sale.order' and \
+                not self.env.user.has_group('base.group_user'):
+            self = self.sudo()
+        return super(MailThread, self)._message_auto_subscribe_notify(
+            partner_ids)
--- portal_sale_distributor/models/sale_order.py
+++ portal_sale_distributor/models/sale_order.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields
+from odoo import models, fields, api, _
 
 
 class SaleOrder(models.Model):
@@ -12,3 +12,16 @@ class SaleOrder(models.Model):
         groups="base.group_user,"
         "portal_sale_distributor.group_portal_distributor"
     )
+
+    @api.multi
+    def action_confirm_distributor(self):
+        self.message_post(
+            _("Pedido confirmado por %s") % self.env.user.name,
+            subtype='mt_comment')
+        self = self.sudo()
+        return self.action_confirm()
+
+    @api.multi
+    def print_quotation_distributor(self):
+        self = self.sudo()
+        return self.print_quotation()
