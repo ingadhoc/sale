6d55e0e880b54fbd47616ffbd69f0f1689ea8cc3
nicomacr <nmr@adhoc.com.ar>
2019-01-15 11:47:51 -0300

    [IMP] some improvement to sale order type in account invoice

--- sale_order_type_ux/README.rst
+++ sale_order_type_ux/README.rst
@@ -19,6 +19,8 @@ Sale Order Type Ux
 * Also add tracking to same field.
 * Auto-complete the same Analytic Tag setted on order type.
 * Move sale type field after date instead of after currency
+* Add new behaivor to the onchage sale type in invoice, to change the company.
+* Set in Invoice view form the field "Sale Type" readonly to states different than "draft".
 * Integration betweeen Portal and Sale Order Type module:
 
  - New access record to sale type model for portal users
---
+++ sale_order_type_ux/models/account_invoice.py
@@ -0,0 +1,21 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+
+from odoo import api, fields, models
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    sale_type_id = fields.Many2one(
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+    )
+
+    @api.onchange('sale_type_id')
+    def onchange_sale_type_id(self):
+        super(AccountInvoice, self).onchange_sale_type_id()
+        if self.sale_type_id.journal_id:
+            self.company_id = self.sale_type_id.journal_id.company_id.id
