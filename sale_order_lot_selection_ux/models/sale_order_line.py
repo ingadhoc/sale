60e02fb6eb23eb4f77fb6aeb8f5cf14d16b1e2d1
nicomacr <nmr@adhoc.com.ar>
2019-04-09 09:40:50 -0300

    [ADD] sale_order_lot_selection_ux: new module extended from sale_order_lot_selection

---
+++ sale_order_lot_selection_ux/models/sale_order_line.py
@@ -0,0 +1,29 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import api, fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    available_lot_ids = fields.Many2many(
+        'stock.production.lot',
+        compute="_compute_available_lot_ids",
+    )
+
+    @api.depends('product_id')
+    def _compute_available_lot_ids(self):
+        for rec in self.filtered(
+                lambda x: x.product_id.tracking
+                in ['serial', 'lot'] and x.order_id.warehouse_id):
+            location = rec.order_id.warehouse_id.lot_stock_id
+            quants = self.env['stock.quant'].read_group([
+                ('product_id', '=', rec.product_id.id),
+                ('location_id', 'child_of', location.id),
+                ('quantity', '>', 0),
+                ('lot_id', '!=', False),
+            ], ['lot_id'], 'lot_id')
+            available_lot_ids = [quant['lot_id'][0] for quant in quants]
+            rec.available_lot_ids = available_lot_ids
