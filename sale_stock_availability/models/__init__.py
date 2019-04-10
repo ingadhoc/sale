f948380e023e4b20baf1b2d7f5d273f6fae27bad
Valentino <vd@adhoc.com.ar>
2019-04-10 10:47:20 -0300

    [FIX] Moved to 'sale_stock_ux'.

--- sale_stock_availability/models/__init__.py
+++ sale_stock_availability/models/__init__.py
@@ -2,5 +2,4 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import stock_warehouse
 from . import sale_order_line
