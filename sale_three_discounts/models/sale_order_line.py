6959d465ce4a1fd35d04cfdab8788491aaabcbe1
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2019-06-13 17:48:14 -0300

    [FIX] sale_three_discounts: remove readonly to the 3 discounts fields (#229)

--- sale_three_discounts/models/sale_order_line.py
+++ sale_three_discounts/models/sale_order_line.py
@@ -14,20 +14,14 @@ class SaleOrderLine(models.Model):
     discount1 = fields.Float(
         'Discount 1 (%)',
         digits=dp.get_precision('Discount'),
-        readonly=True,
-        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
     )
     discount2 = fields.Float(
         'Discount 2 (%)',
         digits=dp.get_precision('Discount'),
-        readonly=True,
-        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
     )
     discount3 = fields.Float(
         'Discount 3 (%)',
         digits=dp.get_precision('Discount'),
-        readonly=True,
-        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
     )
     # TODO do like in invoice line? Make normal field with constraint and
     # oncahnge?
