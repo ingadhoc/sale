0c3bb9876952de5735eb8f67b27135d48f5e053e
Valentino <valentinodefelice@gmail.com>
2019-07-17 09:31:41 -0300

    [FIX] blank space in view.

--- sale_order_type_ux/migrations/11.0.1.2.0/post-migration.py
+++
@@ -1,8 +0,0 @@
-from openupgradelib import openupgrade
-
-
-@openupgrade.migrate()
-def migrate(env, version):
-    openupgrade.load_data(
-        env.cr, 'sale_order_type_ux',
-        'migrations/11.0.1.2.0/mig_data.xml')
