870f69129bcbc4aa629356f291a7d18aa578a7a6
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2019-08-06 08:41:41 -0300

    [REM] remove because it's now in odoo v12

--- sale_stock_ux/__manifest__.py
+++ sale_stock_ux/__manifest__.py
@@ -38,7 +38,6 @@
     'data': [
         'views/sale_order_views.xml',
         'views/sale_order_line_views.xml',
-        'views/product_template_views.xml',
         'views/product_product_views.xml',
         'views/stock_warehouse_views.xml',
         'views/stock_move_views.xml',
