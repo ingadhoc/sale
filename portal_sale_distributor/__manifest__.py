b4f905abff0e828889241014662758f18e26a0a5
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2019-08-13 08:35:18 -0300

    [ADD] portal_sale_distributor_sale: Improvement to portal invoices
    ADD in the portal view of invoice, an table with the payment lines with amount, date and number of the payment
    ADD filter in portal invoices to filter all or open invoices

--- portal_sale_distributor/__manifest__.py
+++ portal_sale_distributor/__manifest__.py
@@ -40,6 +40,7 @@
         'views/portal_sale_views.xml',
         'views/portal_my_account_views.xml',
         'views/product_product_views.xml',
+        'views/portal_account_templates.xml',
         'views/portal_templates.xml',
     ],
     'installable': True,
