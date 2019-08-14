2525adc38de54a5feea90cebebb2a8e7cdd194c1
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2019-08-14 12:22:47 -0300

    [ADD] portal_sale_distributor: Some changes in portal_sale_distributor
    Move the menu products to new icon app and related with product template.

--- portal_sale_distributor/models/__init__.py
+++ portal_sale_distributor/models/__init__.py
@@ -5,4 +5,4 @@
 from . import sale_order
 from . import sale_order_line
 from . import mail_thread
-from . import product_product
+from . import product_template
--- portal_sale_distributor/models/product_template.py
+++ portal_sale_distributor/models/product_template.py
@@ -6,16 +6,16 @@ from odoo import models
 from odoo.tools import pycompat
 
 
-class ProductProduct(models.Model):
-    _inherit = 'product.product'
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
 
-    def _compute_product_price(self):
+    def _compute_template_price(self):
         # other approach could be to inherit the method that builds the
         # action (like fields view get)
-        super(ProductProduct, self)._compute_product_price()
+        super()._compute_template_price()
         if self._context.get('portal_products'):
-            pricelist = self.env.user.partner_id.property_product_pricelist.id
-            context = dict(self._context, pricelist=pricelist,
+            pricelist = self.env.user.partner_id.property_product_pricelist
+            context = dict(self._context, pricelist=pricelist.id,
                            partner=self.env.user.partner_id)
             self2 = self.with_context(
                 context) if self._context != context else self
