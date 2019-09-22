9b992f9b5253b496b32e71f38ccfc686f3e38f64
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-09-22 20:24:14 -0300

    Revert "[REM] remove deprecated sale_timesheet_ux"
    This reverts commit 9acc010181e829b7ec62ae949c049312dc71050d.

---
+++ sale_timesheet_ux/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Sales Timesheet UX",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "website": "www.adhoc.com.ar",
+    "license": "AGPL-3",
+    "author": "ADHOC SA",
+    "category": "sale",
+    "depends": [
+        "sale_timesheet",
+    ],
+    "data": [
+        "views/project_project_views.xml",
+    ],
+    "demo": [
+    ],
+    "application": False,
+    "installable": True,
+}
