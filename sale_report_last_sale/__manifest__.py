3915af5dba5919403637d1c132a7c345a6c7b158
Daniel Libonati <dl@adhoc.com.ar>
2019-11-19 15:32:17 -0300

    [REL] sale:sale_report_last_sale: Migration to v12 (#286)
    * Made installable
    
    * File formatted. Added names to filters.
    
    * Added ADHOC SA to authors

--- sale_report_last_sale/__manifest__.py
+++ sale_report_last_sale/__manifest__.py
@@ -9,7 +9,8 @@
         'sale',
     ],
     'author': 'Ivan Todorovich,'
-              'Odoo Community Association (OCA)',
+              'Odoo Community Association (OCA),'
+              'ADHOC SA',
     'license': 'AGPL-3',
     'data': [
         'reports/sale_last_sale_report.xml',
@@ -17,5 +18,5 @@
         'security/ir_rule.xml',
     ],
     'category': 'Sale',
-    'installable': False,
+    'installable': True,
 }
