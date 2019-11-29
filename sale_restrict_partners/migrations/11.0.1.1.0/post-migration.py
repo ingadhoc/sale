0a0487bc161087974f42a90aef38545a013ce296
Lucas L. Soto <ls@adhoc.com.ar>
2019-11-29 09:38:02 -0300

    [REM] sale_restrict_partners: deprecate module

--- sale_restrict_partners/migrations/11.0.1.1.0/post-migration.py
+++
@@ -1,8 +0,0 @@
-from openupgradelib import openupgrade
-
-
-@openupgrade.migrate()
-def migrate(env, version):
-    openupgrade.load_data(
-        env.cr, 'sale_restrict_partners',
-        'migrations/11.0.1.1.0/mig_data.xml')
