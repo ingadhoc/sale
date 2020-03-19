f4a1e17d1a2756df06db3a1a457e6b0191150eca
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2020-03-19 06:36:20 -0300

    [13.0][IMP] portal_distributor: Add new portal menu "open invoices" (#314)
    * [11.0][IMP] portal_distributor: Add new portal menu "open invoices"
    
    Add new portal item menu analog to account summary, but only with invoices open, not historical.
    
    * FIX flake8 errors

--- portal_sale_distributor/controllers/portal.py
+++ portal_sale_distributor/controllers/portal.py
@@ -37,3 +37,28 @@ class PortalSummary(Controller):
             ('Content-Disposition',
              content_disposition('Resumen de Cuenta' + '.xls'))]
         return request.make_response(xls, headers=xlshttpheaders)
+
+    @http.route(['/my/open_invoices', '/my/open_invoices/page/<int:page>'],
+                type='http', auth="user", website=True)
+    def portal_my_open_invoices(self, **kw):
+        partner = request.env.user.partner_id
+        report_data = {
+            'secondary_currency': False,
+            'financial_amounts': False,
+            'result_selection': 'all',
+            'company_type': 'group_by_company',
+            'company_id': False,
+            'to_date': False,
+            'historical_full': False,
+            'show_invoice_detail': False,
+            'lang': partner.lang,
+        }
+        xls = request.env.ref(
+            'account_debt_management.account_debt_report').sudo(
+        ).with_context(report_data).render([partner.id], data=report_data)[0]
+        xlshttpheaders = [
+            ('Content-Type', 'application/vnd.ms-excel'),
+            ('Content-Length', len(xls)),
+            ('Content-Disposition',
+             content_disposition('Factura Abiertas' + '.xls'))]
+        return request.make_response(xls, headers=xlshttpheaders)
