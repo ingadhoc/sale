35ceecd9639e053bfe3154fe6303eaae5afdc497
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-05-29 14:11:08 -0300

    [MIG] sale_order_type_invoice_policy: Migration to 13.0
    [FIX] Remove unsed model "sale.order"
    [FIX] readpt the changes in account
    [FIX] remove unused api imports

--- sale_order_type_invoice_policy/models/__init__.py
+++ sale_order_type_invoice_policy/models/__init__.py
@@ -2,7 +2,6 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import sale_order
 from . import sale_order_line
 from . import sale_order_type
 from . import stock_picking
