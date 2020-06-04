e350f0e13fced0201f762e043d4a548e6445d1f2
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-06-04 12:25:49 -0300

    [MIG] sale_order_type_user_default: Migration to 13.0

--- sale_order_type_user_default/models/sale_order.py
+++ sale_order_type_user_default/models/sale_order.py
@@ -1,19 +1,11 @@
-from odoo import models, api, fields
+from odoo import models, fields
 
 
 class SaleOrder(models.Model):
 
     _inherit = 'sale.order'
 
-    @api.model
-    def _get_order_type(self):
-        res = super()._get_order_type()
-        user = self.env.user
-        if user.default_sale_order_type_id:
-            res = user.default_sale_order_type_id
-        return res
-
     type_id = fields.Many2one(
         'sale.order.type',
-        default=lambda so: so._get_order_type(),
+        default=lambda so: so.env.user.default_sale_order_type_id or False,
     )
