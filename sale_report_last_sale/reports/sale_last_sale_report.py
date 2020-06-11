593a788a8b33e39ae37ebdf18210118e6450a89f
nmadhoc <nm@adhoc.com.ar>
2020-06-11 14:49:57 -0300

    Se quitó otro api que omití sacar

--- sale_report_last_sale/reports/sale_last_sale_report.py
+++ sale_report_last_sale/reports/sale_last_sale_report.py
@@ -1,6 +1,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models, tools
+from odoo import fields, models, tools
 
 
 class SaleLastSaleReport(models.Model):
