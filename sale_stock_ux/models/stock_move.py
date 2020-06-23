e81f60256a54faa4846a73e949e63602f7097a27
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-06-23 15:42:08 -0300

    [13.0] [REF] sale_stock_ux: Set the notes & observation when change the picking in the moves.
    This is to avoid use write un constrains.

--- sale_stock_ux/models/stock_move.py
+++ sale_stock_ux/models/stock_move.py
@@ -11,3 +11,23 @@ class StockMove(models.Model):
     sale_id = fields.Many2one(
         related='group_id.sale_id',
     )
+
+    def _get_new_picking_values(self):
+        """ return create values for new picking that will be linked with group
+        of moves in self.
+        """
+        res = super()._get_new_picking_values()
+        values = {}
+        sale = self.mapped('group_id.sale_id')
+        propagate_internal_notes = self.env['ir.config_parameter'].sudo(
+        ).get_param('sale.propagate_internal_notes') == 'True'
+        propagate_note = self.env['ir.config_parameter'].sudo(
+        ).get_param('sale.propagate_note') == 'True'
+        if propagate_internal_notes and sale.internal_notes:
+            values['note'] = sale.internal_notes
+        if propagate_note and sale.note:
+            values['observations'] = sale.note
+        if values:
+            res.update(values)
+
+        return res
