f38b506ea1b73609fdd0030e90e24b413ed1d9db
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-06-23 15:42:08 -0300

    [13.0] [REF] sale_stock_ux: Set the notes & observation when change the picking in the moves.
    This is to avoid use write un constrains.

--- sale_stock_ux/models/stock_picking.py
+++ sale_stock_ux/models/stock_picking.py
@@ -1,4 +1,4 @@
-from odoo import models, api, fields
+from odoo import models, fields
 
 
 class StockPicking(models.Model):
@@ -7,19 +7,3 @@ class StockPicking(models.Model):
 
     observations = fields.Text(
     )
-
-    @api.constrains('sale_id')
-    def set_notes(self):
-        """Setamos notas internas y observaciones desde la venta
-        """
-        for rec in self.filtered('sale_id'):
-            vals = {}
-            propagate_internal_notes = self.env['ir.config_parameter'].sudo(
-            ).get_param('sale.propagate_internal_notes') == 'True'
-            propagate_note = self.env['ir.config_parameter'].sudo(
-            ).get_param('sale.propagate_note') == 'True'
-            if propagate_internal_notes and rec.sale_id.internal_notes:
-                vals['note'] = rec.sale_id.internal_notes
-            if propagate_note and rec.sale_id.note:
-                vals['observations'] = rec.sale_id.note
-            rec.write(vals)
