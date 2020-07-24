5199f9ce809f88ba1f9559ba6cd40adbbc7bce04
nmadhoc <nm@adhoc.com.ar>
2020-07-24 15:23:42 -0300

    [MIG] sale_order_lot_selection_ux: Migration to 13.0

--- sale_order_lot_selection_ux/__manifest__.py
+++ sale_order_lot_selection_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Sale Order Lot Selection UX',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'category': 'Sale',
     'sequence': 14,
     'author': 'ADHOC SA',
@@ -31,7 +31,7 @@
     'data': [
         'views/sale_order_views.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
