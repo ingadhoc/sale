a400fefeb87551f97c431288663165cbeda80490
nmadhoc <nm@adhoc.com.ar>
2020-11-03 13:26:36 -0300

    [ADD] crm_stage_teams

---
+++ crm_stage_teams/__manifest__.py
@@ -0,0 +1,25 @@
+# © 2016 ADHOC SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'CRM Stage Teams',
+    'version': '12.0.1.1.0',
+    'sequence': 14,
+    'summary': '',
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'license': 'AGPL-3',
+    'images': [
+    ],
+    'depends': [
+        'crm',
+    ],
+    'data': [
+        'views/crm_stage_views.xml',
+        'views/crm_lead_views.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ crm_stage_teams/models/crm_lead.py
@@ -0,0 +1,24 @@
+from odoo import api, fields, models, SUPERUSER_ID
+
+
+class Lead(models.Model):
+    _inherit = 'crm.lead'
+
+    stage_id = fields.Many2one(
+        'crm.stage', string='Stage', ondelete='restrict',
+        track_visibility='onchange', index=True, copy=False,
+        domain="['|', ('team_ids', '=', False), ('team_ids', '=', team_id)]",
+        group_expand='_read_group_stage_ids', default=lambda self: self._default_stage_id())
+
+    @api.model
+    def _read_group_stage_ids(self, stages, domain, order):
+        team_id = self._context.get('default_team_id')
+        if team_id:
+            search_domain = [
+                '|', ('id', 'in', stages.ids), '|', ('team_ids', '=', False),
+                ('team_ids', 'in', [team_id])]
+        else:
+            search_domain = ['|', ('id', 'in', stages.ids), ('team_ids', '=', False)]
+        # perform search
+        stage_ids = stages._search(search_domain, order=order, access_rights_uid=SUPERUSER_ID)
+        return stages.browse(stage_ids)
