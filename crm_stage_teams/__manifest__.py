c5e332d09c521b0a78c6a19637a80c17cc0c4237
nmadhoc <nm@adhoc.com.ar>
2020-12-21 09:50:14 -0300

    [MIG] crm_stage_teams/: Migration to 13.0

--- crm_stage_teams/__manifest__.py
+++ crm_stage_teams/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'CRM Stage Teams',
-    'version': '12.0.1.1.0',
+    'version': '13.0.1.0.0',
     'sequence': 14,
     'summary': '',
     'author': 'ADHOC SA',
