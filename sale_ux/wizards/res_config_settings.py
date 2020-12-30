5c05ab21d3667a99a1d7aa3ff742c79a3af7f873
Pablo Paez <pablopaezps97@gmail.com>
2020-12-30 16:17:23 -0300

    [IMP] sale_ux: allow any user as salesman

--- sale_ux/wizards/res_config_settings.py
+++ sale_ux/wizards/res_config_settings.py
@@ -9,6 +9,8 @@ class ResConfigSettings(models.TransientModel):
 
     _inherit = 'res.config.settings'
 
+    group_allow_any_user_as_salesman = fields.Boolean(
+        "Allow any user as salesman", implied_group='sale_ux.group_allow_any_user_as_salesman')
     # agregamos sale para que no se llame igual al de account
     group_sale_reference_on_tree_and_main_form = fields.Boolean(
         'Customer reference in list view and form',
