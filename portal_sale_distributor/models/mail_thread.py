c17acfaab513455d61eaf1aa77c2c5c1c7d8788a
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-01-18 13:56:50 -0300

    [12.0] [FIX] portal_sale_distributor: Avoid problems with the comercial user when create SO.
    After this odoo change https://github.com/odoo/odoo/commit/99d0308d5870088970667744fdd0c818299927bc
    This hack is necessary in the method "message auto subscribe" to make it's works.

--- portal_sale_distributor/models/mail_thread.py
+++ portal_sale_distributor/models/mail_thread.py
@@ -5,7 +5,7 @@ class MailThread(models.AbstractModel):
     _inherit = 'mail.thread'
 
     @api.multi
-    def _message_auto_subscribe_notify(self, partner_ids, template):
+    def _message_auto_subscribe(self, updated_values):
         """ Cuando usuario portal crea OV se manda un mensjae de suscripcion
         al comercial, esto terminahaciendo que se arroje error si se tiene
         instalado mass_mailing y las estadisticas por permiso de acceso
@@ -15,4 +15,5 @@ class MailThread(models.AbstractModel):
         if self._name == 'sale.order' and \
                 not self.env.user.has_group('base.group_user'):
             self = self.sudo()
-        return super(MailThread, self)._message_auto_subscribe_notify(partner_ids, template)
+
+        return super(MailThread, self)._message_auto_subscribe(updated_values)
