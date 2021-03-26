46232b5d6148eec8a0185d38ac0e9e847b12f427
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-03-26 11:12:12 -0300

    [13.0] [IMP] portal_sale_distributor: Only show tree view for products.

--- portal_sale_distributor/__manifest__.py
+++ portal_sale_distributor/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Portal Distributor Sale',
-    'version': '13.0.1.1.0',
+    'version': '13.0.1.3.0',
     'category': 'Tools',
     'complexity': 'easy',
     'author': 'ADHOC SA, Odoo Community Association (OCA)',
