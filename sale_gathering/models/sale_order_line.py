7a725bdbbec6f76b1b48fc56ec66944531ad5235
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-05-06 19:44:15 +0000

    [IMP] sale_gathering: New option to generate only one invoice per downpayment and set the invoice to zero.
    Part-of: ingadhoc/sale#432

--- sale_gathering/models/sale_order_line.py
+++ sale_gathering/models/sale_order_line.py
@@ -16,7 +16,13 @@ class SaleOrderLine(models.Model):
 
     def _prepare_invoice_line(self):
         result = super()._prepare_invoice_line()
-        if self.is_downpayment and self._context.get('invoice_ids', False):
-            invoices = self.env['account.move'].browse(self._context.get('invoice_ids'))
-            result['price_unit'] = sum(invoices.mapped('amount_untaxed'))
+        # if self.is_downpayment and self._context.get('invoice_gathering', False):
+        #     invoices = self.env['account.move'].browse(self._context.get('invoice_ids'))
+        #     result['price_unit'] = sum(invoices.mapped('amount_untaxed'))
+        #     result['quantity'] = 1.0
+        if self.is_downpayment and self._context.get('invoice_gathering', False):
+            lines = self.order_id.order_line.filtered(lambda x: not x.is_downpayment and x.qty_to_invoice)
+            result['price_unit'] = lines and sum(lines.mapped('untaxed_amount_to_invoice')) or 0.0
+            result['quantity'] = -1.0
         return result
+
