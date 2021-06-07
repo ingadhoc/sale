873761e7d58029bb8352b0ffe92450a1531e1a5a
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-06-07 09:15:48 -0300

    [IMP] new approach to invoice in zero with taxes and restrict when amount gathering is negative.
    X-original-commit: fa01d180f82cf4a8bc79b459f18b940c8673b498

--- sale_gathering/models/sale_order.py
+++ sale_gathering/models/sale_order.py
@@ -1,4 +1,6 @@
-from odoo import models, fields
+from odoo import models, fields, api, _
+from odoo.tools import float_compare
+from odoo.exceptions import ValidationError
 
 
 class SaleOrder(models.Model):
@@ -7,27 +9,47 @@ class SaleOrder(models.Model):
 
     is_gathering = fields.Boolean('Is Gathering?')
 
-    amount_gathering = fields.Float(compute="_compute_amount_gathering")
+    gathering_balance = fields.Float(
+        compute="_compute_gathering_balance",
+        digits='Product Price',
+    )
 
-    def _compute_amount_gathering(self):
-        for rec in self:
-            # amount_lines = sum(rec.order_line.filtered(
-            #     lambda x: not x.is_downpayment).mapped(
-            #     lambda x: x.qty_invoiced * x.price_unit))
-            amount_to_invoice = sum(rec.order_line.filtered(
-                lambda x: not x.is_downpayment).mapped('untaxed_amount_to_invoice'))
-            amount_invoiced = sum(rec.order_line.filtered(
-                lambda x: not x.is_downpayment).mapped('untaxed_amount_invoiced'))
-            rec.amount_gathering = sum(
-                rec.order_line.filtered('is_downpayment').mapped('price_unit')) - amount_invoiced - amount_to_invoice
-
+    @api.depends(
+        'is_gathering',
+        'order_line.price_unit_with_tax',
+        'order_line.qty_invoiced',
+        'order_line.is_downpayment',
+    )
+    def _compute_gathering_balance(self):
+        self.gathering_balance = 0.0
+        for rec in self.filtered('is_gathering'):
+            amount_to_invoice = sum(
+                rec.order_line.filtered(lambda x: not x.is_downpayment).mapped(
+                    lambda l: l.price_reduce_taxinc * l.qty_to_invoice))
+            amount_invoiced = sum(
+                rec.order_line.filtered(lambda x: not x.is_downpayment).mapped(
+                    lambda l: l.price_reduce_taxinc * l.qty_invoiced))
+            rec.gathering_balance = sum(
+                rec.order_line.filtered('is_downpayment').mapped(
+                    'price_unit_with_tax')) - amount_invoiced - amount_to_invoice
 
     def _get_invoiceable_lines(self, final=False):
         """Return the invoiceable lines for order `self`."""
         invoiceable_lines = super()._get_invoiceable_lines(final=False)
-        if self.is_gathering and self.amount_gathering > 0.0:
+        if self.is_gathering and self.gathering_balance > 0.0:
             for line in self.order_line.filtered('is_downpayment'):
                 if final:
                     invoiceable_lines |= line
         return invoiceable_lines
 
+    @api.constrains('is_gathering', 'amount_total')
+    def _check_gathering_balance(self):
+        product_precision_digits = self.env['decimal.precision'].precision_get(
+            'Product Price')
+        for rec in self.filtered('is_gathering'):
+            if float_compare(rec.gathering_balance, 0.0, precision_digits=product_precision_digits) == -1:
+                raise ValidationError(
+                    _(
+                        "The gathering balance will be negative (%s), you cannot make this modification"
+                        " to the order. Order: %s" %
+                        (rec.gathering_balance, rec.name)))
--- sale_gathering/models/sale_order_line.py
+++ sale_gathering/models/sale_order_line.py
@@ -5,7 +5,6 @@ class SaleOrderLine(models.Model):
 
     _inherit = 'sale.order.line'
 
-
     @api.onchange('product_uom', 'product_uom_qty')
     def product_uom_change(self):
         price_unit = self.price_unit
@@ -16,13 +15,15 @@ class SaleOrderLine(models.Model):
 
     def _prepare_invoice_line(self):
         result = super()._prepare_invoice_line()
-        # if self.is_downpayment and self._context.get('invoice_gathering', False):
-        #     invoices = self.env['account.move'].browse(self._context.get('invoice_ids'))
-        #     result['price_unit'] = sum(invoices.mapped('amount_untaxed'))
-        #     result['quantity'] = 1.0
-        if self.is_downpayment and self._context.get('invoice_gathering', False):
-            lines = self.order_id.order_line.filtered(lambda x: not x.is_downpayment and x.qty_to_invoice)
-            result['price_unit'] = lines and sum(lines.mapped('untaxed_amount_to_invoice')) or 0.0
+        if self.is_downpayment and self._context.get(
+                'invoice_gathering', False):
+            lines = self.order_id.order_line.filtered(
+                lambda x: not x.is_downpayment and x.qty_to_invoice)
+            price_subtotal_incl = lines and sum(lines.mapped(
+                lambda l: l.qty_to_invoice * l.price_reduce_taxinc)) or 0.0
+            result['price_unit'] = self.tax_id.with_context(
+                force_price_include=True).compute_all(
+                price_subtotal_incl, currency=self.order_id.currency_id)[
+                'total_excluded']
             result['quantity'] = -1.0
         return result
-
