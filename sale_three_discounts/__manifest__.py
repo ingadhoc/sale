d5cb6a9c064750d30df80e8fc412b24ee0d87e4d
nicolas <nmr@adhoc.com.ar>
2021-09-20 09:00:53 -0300

    [13.0] [FIX] sale_three_discounts: Adapt the module to works with out invoice.

--- sale_three_discounts/__manifest__.py
+++ sale_three_discounts/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Sale Three Discounts',
-    'version': '13.0.1.0.0',
+    'version': '13.0.1.1.0',
     'category': 'Sales Management',
     'sequence': 14,
     'author': 'ADHOC SA',
