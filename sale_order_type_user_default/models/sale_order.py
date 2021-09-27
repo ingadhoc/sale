a7a6436cdbf420c4239c7b91452495399ad770a1
nicolas <nmr@adhoc.com.ar>
2021-09-27 09:39:57 -0300

    [13.0] [FIX] sale_order_type_user_default: Fix default value.
    Due the computed stored fields the default value is overridden by the computed method.

--- sale_order_type_user_default/models/sale_order.py
+++ sale_order_type_user_default/models/sale_order.py
@@ -8,3 +8,18 @@ class SaleOrder(models.Model):
     @api.model
     def _default_type_id(self):
         return self.env.user.default_sale_order_type_id or super()._default_type_id()
+
+    def _compute_sale_type_id(self):
+        sales = self.env['sale.order']
+        user_type = self.env.user.default_sale_order_type_id
+        if user_type:
+            for rec in self:
+                # use default user type if:
+                # 1. type dont have company or is same as sale order
+                # 2. there is no default type on partner or commercial partner
+                if (not user_type.company_id or user_type.company_id == rec.company_id) and not \
+                        rec.partner_id.with_context(force_company=rec.company_id.id).sale_type and not \
+                        rec.partner_id.commercial_partner_id.with_context(force_company=rec.company_id.id).sale_type:
+                    sales += rec
+                    rec.type_id = self.env.user.default_sale_order_type_id
+        super(SaleOrder, self - sales)._compute_sale_type_id()
