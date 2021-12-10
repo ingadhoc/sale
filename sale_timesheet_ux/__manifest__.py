ca74323b69b3de591ee77f0e552cae39be2b2a4a
nicolas <nmr@adhoc.com.ar>
2021-12-10 15:36:42 -0300

    [MIG] sale_timesheet_ux: Migration to 15.0

--- sale_timesheet_ux/__manifest__.py
+++ sale_timesheet_ux/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Sales Timesheet UX",
     "summary": "",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "website": "www.adhoc.com.ar",
     "license": "AGPL-3",
     "author": "ADHOC SA",
@@ -10,10 +10,9 @@
         "sale_timesheet",
     ],
     "data": [
-        "views/project_project_views.xml",
     ],
     "demo": [
     ],
     "application": False,
-    'installable': False,
+    'installable': True,
 }
