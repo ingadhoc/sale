3f27ccef3c25726981b411032d77ee0f1fa1bd70
Pablo Paez <pp@adhoc.com.ar>
2021-12-21 17:07:38 -0300

    [MIG] sale_quotation_products: Migration to 15.0

--- sale_quotation_products/__manifest__.py
+++ sale_quotation_products/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Sale Quotation Products',
-    'version': "13.0.1.1.0",
+    'version': "15.0.1.0.0",
     'category': 'Sales Management',
     'sequence': 14,
     'summary': '',
@@ -29,14 +29,14 @@
     'images': [
     ],
     'depends': [
-        'sale',
+        'sale_management',
     ],
     'data': [
         'views/sale_order_views.xml',
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
