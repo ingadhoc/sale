f95030aa08a0ab597d96c9cf7b37c359350ba65e
Pablo Montenegro <pam@adhoc.com.ar>
2022-03-15 13:48:45 -0300

    [FIX]sale_exception_credit_limit: delete constains
    @api.constrains('credit_limit') evita que pueda crearse nuevos usuarios
    desde el website, se elimina debido a la urgencia del cliente
    pero queda pendiente corregir este tema

--- sale_exception_credit_limit/models/res_partner.py
+++ sale_exception_credit_limit/models/res_partner.py
@@ -5,7 +5,6 @@ from odoo.exceptions import ValidationError
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    @api.constrains('credit_limit')
     def change_credit_limit(self):
         if not self.env.user.has_group('sale_exception_credit_limit.credit_config'):
             raise ValidationError(_('You are not allowed to edit credit limit field'))
