3fd02e2903e019d942cadaf716d058ee00c58560
nicolas <nmr@adhoc.com.ar>
2022-07-19 11:41:16 +0000

    [IMP] sale_timesheet_ux: new button to access to the project's update.
    closes ingadhoc/sale#552
    
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

--- sale_timesheet_ux/__manifest__.py
+++ sale_timesheet_ux/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Sales Timesheet UX",
     "summary": "",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.1.0",
     "website": "www.adhoc.com.ar",
     "license": "AGPL-3",
     "author": "ADHOC SA",
@@ -10,6 +10,7 @@
         "sale_timesheet",
     ],
     "data": [
+        "views/project_project_views.xml",
     ],
     "demo": [
     ],
