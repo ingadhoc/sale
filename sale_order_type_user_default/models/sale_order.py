d863035fbe752309b2e59632e5d9b381093e3c8d
Virginia Bonservizi <vib@adhoc.com.ar>
2022-07-21 15:01:26 -0300

    [MIG] sale_order_type_user_default: Migration to 15.0

--- sale_order_type_user_default/models/sale_order.py
+++ sale_order_type_user_default/models/sale_order.py
@@ -18,8 +18,8 @@ class SaleOrder(models.Model):
                 # 1. type dont have company or is same as sale order
                 # 2. there is no default type on partner or commercial partner
                 if (not user_type.company_id or user_type.company_id == rec.company_id) and not \
-                        rec.partner_id.with_context(force_company=rec.company_id.id).sale_type and not \
-                        rec.partner_id.commercial_partner_id.with_context(force_company=rec.company_id.id).sale_type:
+                        rec.partner_id.with_company(rec.company_id.id).sale_type and not \
+                        rec.partner_id.commercial_partner_id.with_company(rec.company_id.id).sale_type:
                     sales += rec
                     rec.type_id = self.env.user.default_sale_order_type_id
         super(SaleOrder, self - sales)._compute_sale_type_id()
