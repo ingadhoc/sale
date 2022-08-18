2b5e544c9ce9d1a97270a6a23549b5bf89a90bc4
Juani Rivero <jr@adhoc.com.ar>
2022-08-18 16:14:37 -0300

    [IMP]sale_timesheet_ux: deleting account_analytic_line.py

--- sale_timesheet_ux/__manifest__.py
+++ sale_timesheet_ux/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Sales Timesheet UX",
     "summary": "",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.1.0",
     "website": "www.adhoc.com.ar",
     "license": "AGPL-3",
     "author": "ADHOC SA",
