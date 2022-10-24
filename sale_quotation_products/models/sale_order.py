558f1a29c0c95d05acc9e88f172348e841343f08
Virginia Bonservizi <vib@adhoc.com.ar>
2022-10-24 16:12:24 -0300

    [FIX] Change actions using _for_xml_id

--- sale_quotation_products/models/sale_order.py
+++ sale_quotation_products/models/sale_order.py
@@ -11,31 +11,28 @@ class SaleOrder(models.Model):
 
     def add_products_to_quotation(self):
         self.ensure_one()
-        action_read = False
-        actions = self.env.ref('product.product_normal_action_sell')
-        if actions:
-            action_read = actions.sudo().read()[0]
-            context = safe_eval(action_read['context'])
+        action_read = self.env["ir.actions.actions"]._for_xml_id('product.product_normal_action_sell')
+        context = safe_eval(action_read['context'])
+        context.update(dict(
+            # we send by context to show the price of product pack detailed for component if exist
+            sale_quotation_products=True,
+            whole_pack_price=True,
+            pricelist=self.pricelist_id.display_name,
+            # we send company in context so it filters taxes
+            company_id=self.company_id.id,
+            partner_id=self.partner_id.id,
+        ))
+        # we do this apart because we need to ensure "warehouse_id" exists in datebase, if for the case that
+        # we don't have inventory installed yet
+        if 'warehouse_id' in self._fields:
             context.update(dict(
-                # we send by context to show the price of product pack detailed for component if exist
-                sale_quotation_products=True,
-                whole_pack_price=True,
-                pricelist=self.pricelist_id.display_name,
-                # we send company in context so it filters taxes
-                company_id=self.company_id.id,
-                partner_id=self.partner_id.id,
+                search_default_location_id=self.warehouse_id.lot_stock_id.id,
             ))
-            # we do this apart because we need to ensure "warehouse_id" exists in datebase, if for the case that
-            # we don't have inventory installed yet
-            if 'warehouse_id' in self._fields:
-                context.update(dict(
-                    search_default_location_id=self.warehouse_id.lot_stock_id.id,
-                ))
-            action_read.update(
-                context=context,
-                name=_('Quotation Products'),
-            )
-            action_read['context'] = context
+        action_read.update(
+            context=context,
+            name=_('Quotation Products'),
+        )
+        action_read['context'] = context
         return action_read
 
     def add_products(self, product_ids, qty):
