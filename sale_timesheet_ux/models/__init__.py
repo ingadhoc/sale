3231da4fd950532f5f600c10b8c726f6119c74e9
matiasperalta1 <matias_peralta1@hotmail.com>
2022-12-02 16:51:48 +0000

    [MIG] sale_timesheet_ux: Migration to 16.0
    closes ingadhoc/sale#599
    
    Signed-off-by: Juan Ignacio Rivero <jr@adhoc.com.ar>

--- sale_timesheet_ux/models/__init__.py
+++ sale_timesheet_ux/models/__init__.py
@@ -1,4 +1,5 @@
 from . import project_task
 from . import project_project
 from . import sale_order_line
+from . import sale_order
 from . import account_analytic_line
--- sale_timesheet_ux/models/project_project.py
+++ sale_timesheet_ux/models/project_project.py
@@ -7,7 +7,10 @@ class ProjectProject(models.Model):
 
     def write(self, values):
         """ Ver readme de modulo item "c"
-        TODO this should go as PR to odoo """
+        # TODO this should go as PR to odoo """
+        for rec in self.task_ids:
+            if self.allow_billable:
+                rec.order_id = rec.sale_order_id
         if 'allow_billable' in values and not values.get('allow_billable'):
             self = self.with_context(write_allow_billiable=True)
         res = super().write(values)
