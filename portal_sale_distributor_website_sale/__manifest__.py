08a5b3e05a397e619931bc0e9445f0c86928ac5d
Virginia Bonservizi <vib@adhoc.com.ar>
2022-12-06 10:01:32 -0300

    [IMP] hide fields in product.template tree views for group_portal_distributor

--- portal_sale_distributor_website_sale/__manifest__.py
+++ portal_sale_distributor_website_sale/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Portal Distributor Website Sale',
-    'version': "15.0.1.0.0",
+    'version': "15.0.1.1.0",
     'category': 'Tools',
     'complexity': 'easy',
     'author': 'ADHOC SA, Odoo Community Association (OCA)',
@@ -34,6 +34,7 @@
     'data': [
         'views/portal_templates.xml',
         'views/templates.xml',
+        'views/product_template_views.xml',
     ],
     'installable': True,
     'auto_install': True,
