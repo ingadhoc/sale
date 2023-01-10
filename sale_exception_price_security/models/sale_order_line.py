01aa9b8218c7d3b7d2090451ca76c1f6efe82850
Virginia Bonservizi <vib@adhoc.com.ar>
2023-01-10 12:45:58 +0000

    [MIG] sale_exception_price_security: Migration to 16.0
    closes ingadhoc/sale#620
    
    Related: ingadhoc/product#488
    Signed-off-by: Juan Ignacio Rivero <jr@adhoc.com.ar>

--- sale_exception_price_security/models/sale_order_line.py
+++ sale_exception_price_security/models/sale_order_line.py
@@ -15,7 +15,7 @@ class SaleOrderLine(models.Model):
     def check_discount_ok(self):
         self.ensure_one()
         # disable constrant
-        if (self.user_has_groups('price_security.group_restrict_prices'
+        if (self.user_has_groups('price_security.group_only_view'
                                  ) and not self.product_can_modify_prices):
             # if something, then we have an error, not ok
             if self.env.user.check_discount(
