e62c7d27d53f42d5a1d486f00b615698905db53a
Joel Zilli <joz@adhoc.com.ar>
2023-02-01 08:34:17 -0300

    [FIX]sale: changes of logic in model project_project

--- sale_timesheet_ux/models/project_project.py
+++ sale_timesheet_ux/models/project_project.py
@@ -8,10 +8,12 @@ class ProjectProject(models.Model):
     def write(self, values):
         """ Ver readme de modulo item "c"
         # TODO this should go as PR to odoo """
-        for rec in self.task_ids:
-            if self.allow_billable:
-                rec.order_id = rec.sale_order_id
         if 'allow_billable' in values and not values.get('allow_billable'):
             self = self.with_context(write_allow_billiable=True)
         res = super().write(values)
+        if 'allow_billable' in values:
+             for rec in self:
+                if rec.allow_billable:
+                    for task in rec.task_ids:
+                        task.order_id = task.sale_order_id
         return res
