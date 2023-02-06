a342dbddf43b0cebea389933d04ba58bf2b1c1e8
Juani Rivero <jr@adhoc.com.ar>
2023-02-06 16:01:02 +0000

    [FIX]sale_barcode: order line creation on scan
    closes ingadhoc/sale#631
    
    Signed-off-by: jok-adhoc <jok@adhoc.com.ar>

--- sale_barcode/static/src/js/sale_barcode_handler.js
+++ sale_barcode/static/src/js/sale_barcode_handler.js
@@ -1,17 +1,13 @@
 odoo.define('sale_barcode.SaleBarcodeHandler', function (require) {
 "use strict";
 
-var core = require('web.core');
 var AbstractField = require('web.AbstractField');
-var field_registry = require('web.field_registry');
-
-var _t = core._t;
+var fieldRegistry = require('web.field_registry');
 
 
 var SaleBarcodeHandler = AbstractField.extend({
     init: function() {
         this._super.apply(this, arguments);
-
         this.trigger_up('activeBarcode', {
             name: this.name,
             fieldName: 'order_line',
@@ -25,7 +21,8 @@ var SaleBarcodeHandler = AbstractField.extend({
     },
 });
 
-field_registry.add('sale_barcode_handler', SaleBarcodeHandler);
+
+fieldRegistry.add('sale_barcode_handler', SaleBarcodeHandler);
 
 return SaleBarcodeHandler;
 
