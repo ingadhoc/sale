23ebbab7b4bf0c0f8dfcf7f152be859c4df2b98f
matiasperalta1 <matias_peralta1@hotmail.com>
2023-02-28 14:32:09 -0300

    [FIX] crm_teams_ux: domain of user_id fixed

--- crm_teams_ux/__manifest__.py
+++ crm_teams_ux/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'CRM Teams UX',
-    'version': "16.0.1.0.0",
+    'version': "16.0.2.0.0",
     'sequence': 14,
     'summary': '',
     'author': 'ADHOC SA',
