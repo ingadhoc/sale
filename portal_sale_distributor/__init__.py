91def67d3952f9fab97fd0698ed488f786860d52
Juan Jose Scarafia <jjs@adhoc.com.ar>
2023-04-20 09:48:24 -0300

    ammend

--- portal_sale_distributor/__init__.py
+++ portal_sale_distributor/__init__.py
@@ -2,5 +2,6 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
+
 from . import models
 from . import controllers
--- sale_exception_credit_limit/models/__init__.py
+++ sale_exception_credit_limit/models/__init__.py
@@ -1,5 +1,6 @@
 ##############################################################################
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
+
 ##############################################################################
 from . import sale_order
