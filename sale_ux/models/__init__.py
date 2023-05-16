ccee855c66ed5ff1ec2cfc7a222677a72250c124
Juani Rivero <jr@adhoc.com.ar>
2023-05-16 10:21:20 -0300

    [FIX]Revert PR 660

--- sale_ux/models/__init__.py
+++ sale_ux/models/__init__.py
@@ -2,7 +2,6 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import crm_lead
 from . import sale_order
 from . import sale_order_line
 from . import account_move
