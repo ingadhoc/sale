cb99c5ddb726240f429700f8301ea27a8fd5fbbe
Virginia Bonservizi <vib@adhoc.com.ar>
2023-06-13 14:09:59 -0300

    [MIG] portal_sale_distributor: Migration to 16.0

--- portal_sale_distributor/models/__init__.py
+++ portal_sale_distributor/models/__init__.py
@@ -2,6 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import sale_order
-from . import mail_thread
 from . import ir_action_act_window
+from . import mail_thread
+from . import product_product
+from . import sale_order
