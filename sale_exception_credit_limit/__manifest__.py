21bae7401013984536713ddc74bd3f20a07fc8da
Juani Rivero <jr@adhoc.com.ar>
2023-07-25 19:20:36 +0000

    [FIX]sale_exception_credit_limit: error on ecommerce
    closes ingadhoc/sale#686
    
    Signed-off-by: jok-adhoc <jok@adhoc.com.ar>

--- sale_exception_credit_limit/__manifest__.py
+++ sale_exception_credit_limit/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Sale Exception Credit Limit',
-    'version': "16.0.1.2.0",
+    'version': "16.0.1.3.0",
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
