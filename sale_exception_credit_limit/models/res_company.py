b38e75a3d690702cd09412b01256bbf811c59cca
jok-adhoc <jok@adhoc.com.ar>
2023-07-31 16:33:10 +0000

    [FIX]sale_exception_credit_limit: cambios varios
    Part-of: ingadhoc/sale#690

--- sale_exception_credit_limit/models/res_company.py
+++ sale_exception_credit_limit/models/res_company.py
@@ -2,6 +2,12 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import sale_order
-from . import res_partner
-from . import res_company
+from odoo import models, api
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    @api.model
+    def _post_init_credit(self):
+        self.search([]).write({'account_use_credit_limit':True})
