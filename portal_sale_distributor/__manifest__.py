00fa72ff3d17b8d7667e8b89f31d5b3ea381b7c2
lav-adhoc <lav@adhoc.com.ar>
2023-10-25 15:28:49 -0300

    [IMP] portal_sale_distributor: hide fields in product_tremplate view
    X-original-commit: d136a8431f65923f0e9e21e7cdff35c9e4f92d1b

--- portal_sale_distributor/__manifest__.py
+++ portal_sale_distributor/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Portal Distributor Sale',
-    'version': "16.0.1.1.0",
+    'version': "16.0.1.2.0",
     'category': 'Tools',
     'complexity': 'easy',
     'author': 'ADHOC SA, Odoo Community Association (OCA)',
