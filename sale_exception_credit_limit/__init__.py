f86538f4c1ab9c1f04ef8a8b6f634cd7a7705323
fw-bot-adhoc <93535218+fw-bot-adhoc@users.noreply.github.com>
2023-10-27 22:16:26 -0300

    [FW][FIX]sale_exception_credit_limit: cambios varios (#714)
    * Cherry pick of 4074e6fd31a0b38c1272e545153723bd75229101 failed
    
    stdout:
    
    stderr:
    01:13:20.183547 git.c:444               trace: built-in: git cherry-pick 4074e6fd31a0b38c1272e545153723bd75229101
    error: Cherry-picking is not possible because you have unmerged files.
    hint: Fix them up in the work tree, and then use 'git add/rm <file>'
    hint: as appropriate to mark resolution and make a commit.
    fatal: cherry-pick failed
    ----------
    status:
    
    * Update __manifest__.py
    
    ---------
    
    Co-authored-by: fw-bot-adhoc <>
    Co-authored-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- sale_exception_credit_limit/__init__.py
+++ sale_exception_credit_limit/__init__.py
@@ -3,3 +3,9 @@
 # directory
 ##############################################################################
 from . import models
+from odoo import api, SUPERUSER_ID
+
+
+def _post_init_credit(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env['res.company'].search([]).write({'account_use_credit_limit':True})
--- sale_exception_credit_limit/__manifest__.py
+++ sale_exception_credit_limit/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Sale Exception Credit Limit',
-    'version': "16.0.1.3.0",
+    'version': "16.0.1.4.0",
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
@@ -37,4 +37,5 @@
         'demo/res_partner_demo.xml'
     ],
     'installable': False,
+    'post_init_hook': '_post_init_credit',
 }
