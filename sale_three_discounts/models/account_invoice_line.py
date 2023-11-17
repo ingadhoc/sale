4ba4489b026731fe6415a0710dd9da633c23be4d
jok-adhoc <jok@adhoc.com.ar>
2023-11-17 11:59:31 +0000

    [FIX]sale_three_discounts: discounts on invoice
    closes ingadhoc/sale#734
    
    X-original-commit: cdd2fb253c183f9208203eef5e8f4b0680d63d69
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- sale_three_discounts/models/account_invoice_line.py
+++ sale_three_discounts/models/account_invoice_line.py
@@ -6,7 +6,7 @@ from odoo import fields, models, api, _
 from odoo.exceptions import ValidationError
 
 
-class AccountInvoiceLine(models.Model):
+class AccountMoveLine(models.Model):
 
     _inherit = "account.move.line"
 
@@ -21,6 +21,15 @@ class AccountInvoiceLine(models.Model):
     discount3 = fields.Float(
         'Discount 3 (%)',
         digits='Discount',
+    )
+    # TODO do like in invoice line? Make normal field with constraint and
+    # oncahnge?
+    discount = fields.Float(
+        compute='_compute_discounts',
+        store=True,
+        readonly=True,
+        # agregamos states vacio porque lo hereda de la definicion anterior
+        states={},
     )
 
     @api.constrains('discount1', 'discount2', 'discount3')
@@ -38,15 +47,31 @@ class AccountInvoiceLine(models.Model):
                     ",".join(error) + " must be less or equal than 100"
                 ))
 
-    def _set_discount(self):
+    def write(self, vals):
+        self.inverse_vals([vals])
+        return super().write(vals)
+
+    def inverse_vals(self, vals_list):
+        """ No usamos metodo inverse porque en el create odoo termina llamando
+        a inverse y unificando los descuentos en la primer linea.
+        Además, solo actualizamos con el inverse el primer descuento
+        principalmente por compatibilidad con listas que discriminen descuento
+        y consideramos que las columnas 2 y 3 son descuentos adicionales y no
+        las pisamos
+        """
+        for vals in vals_list:
+            precision = self.env['decimal.precision'].precision_get('Discount')
+            if 'discount' in vals \
+                    and not {'discount1', 'discount2', 'discount3'} & set(vals.keys()):
+                vals.update({
+                    'discount1': vals.get('discount'),
+                })
+
+    @api.depends('discount1', 'discount2', 'discount3')
+    def _compute_discounts(self):
         for rec in self:
             discount_factor = 1.0
             for discount in [rec.discount1, rec.discount2, rec.discount3]:
                 discount_factor = discount_factor * (
                     (100.0 - discount) / 100.0)
             rec.discount = 100.0 - (discount_factor * 100.0)
-
-    @api.depends('quantity', 'discount', 'price_unit', 'tax_ids', 'currency_id', 'discount1', 'discount2', 'discount3')
-    def _compute_totals(self):
-        self.filtered(lambda x: x.move_id.is_sale_document())._set_discount()
-        super()._compute_totals()
