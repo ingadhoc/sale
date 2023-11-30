4c660305c917707478fd3c1bfdbda4716a4130a8
Matias Peralta <matias_peralta1@hotmail.com>
2023-11-30 13:47:19 +0000

    [MIG] sale_ux: Migration to 17.0
    closes ingadhoc/sale#743
    
    Signed-off-by: jok-adhoc <jok@adhoc.com.ar>

--- sale_ux/README.rst
+++ sale_ux/README.rst
@@ -19,25 +19,24 @@ Several Improvements to sales:
 #. Make sale quotations menu not visible.
 #. Hide sale invoicing menu (you can already filter on sales orders menu)
 #. Add filters in the sale order view
-#. Make sale orders menu show all sale records (quotations, and confirmed ones).
-#. Make that, by default, links to sale orders shows "sale" data and not only "quotation" data.
+#. Make sale orders menu show all sale records (quotations, and confirmed ones). Make that, by default, links to sale orders shows "sale" data and not only "quotation" data.
 #. Add option in settings to show "Customer Reference" field on Sale Order tree view and in the main section of the form view.
 #. Add a menu item on Orders for "Sale Order Lines" and improve views with more fields.
-#. Make button "Send by email" also available on sale orders with "locked" state.
-#. Make the "Cancel" button also visible sale orders with done state so that if "Lock confirmed Orders" setting is enabled, sale users can still cancel a sale order that has not been delivered or invoiced yet without having to unlock it.
+#. Allow cancel sales orders in locked state and unlock it after cancelling
 #. Fix in button "Create invoice" in sale orders, to create a refund invoice if the sale order amount it's zero and the line's quantities are negative (because of a return).
 #. Block cancellation of a sale order if there is a related invoice in a state different from "draft" or "cancel".
 #. Customer Preview" button in sale orders, opens the online quotation in a new tab.
-#. Add a wizard on sales orders that allow you to define global discounts to sale orders by percentage.
-#. Makes included/excluded taxes optional on total OV/invoice lines and not related to b2b/b2c taxes.
+#. We rename the field price_subtotal and price_total to "Subtotal" and "Total" respectively in sale.order.form
 #. Add option in Sales settings to update prices automatically.
 #. Add selection field to force "Invoiced" status of a sale order, available only for admin with tec features.
 #. Add options in settings to allow any user as salesman (portal or internal)
 #. Add filter to be able to select an analytic account on sale orders with the same partner/commercial partner as the sale order.
 #. Add tracking to payment terms on sale orders.
-#. Add the field "Internal Notes" in the sales order form and the setting to allow propagating the "Internal Notes" to invoices.
+#. Add the field "Internal Notes" in the sales order form and the setting to allow propagating the "Internal Notes / terms & conditions" to invoices / pickings.
 #. Automatically refresh taxes when changing fiscal position
 #. Do not use fiscal positions on prices. By default, when using fiscal position and taxes included on the price on sale orders, the price used on the sale line will be the price without the tax being replaced. We remove that functionality. There are some issues where this is already discussed in odoo standard. For eg. https://github.com/odoo/odoo/issues/66875
+#. Makes the "update prices" button visible.
+#. Show product image on report
 
 
 Installation
--- sale_ux/models/sale_order.py
+++ sale_ux/models/sale_order.py
@@ -92,7 +92,39 @@ class SaleOrder(models.Model):
             raise UserError(_(
                 "Unable to cancel this sale order. You must first "
                 "cancel related bills and pickings."))
-        return super().action_cancel()
+        if any(order.locked for order in self):
+            #No encontre otra forma de evitar el raise usererror que impide que ordenes se cancelen si el pedido está bloqueado
+            cancel_warning = self._show_cancel_wizard()
+            if cancel_warning:
+                self.ensure_one()
+                template_id = self.env['ir.model.data']._xmlid_to_res_id(
+                    'sale.mail_template_sale_cancellation', raise_if_not_found=False
+                )
+                lang = self.env.context.get('lang')
+                template = self.env['mail.template'].browse(template_id)
+                if template.lang:
+                    lang = template._render_lang(self.ids)[self.id]
+                ctx = {
+                    'default_template_id': template_id,
+                    'default_order_id': self.id,
+                    'mark_so_as_canceled': True,
+                    'default_email_layout_xmlid': "mail.mail_notification_layout_with_responsible_signature",
+                    'model_description': self.with_context(lang=lang).type_name,
+                }
+                self.action_unlock()
+                return {
+                    'name': _('Cancel %s', self.type_name),
+                    'view_mode': 'form',
+                    'res_model': 'sale.order.cancel',
+                    'view_id': self.env.ref('sale.sale_order_cancel_view_form').id,
+                    'type': 'ir.actions.act_window',
+                    'context': ctx,
+                    'target': 'new'
+                }
+            else:
+                return self._action_cancel()
+        else:
+            return super().action_cancel()
 
     @api.constrains('force_invoiced_status')
     def check_force_invoiced_status(self):
@@ -176,7 +208,7 @@ class SaleOrder(models.Model):
         precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
         invoices.filtered(
             lambda i: float_is_zero(i.amount_total, precision_digits=precision) and all(
-                [line.quantity <= 0.0 for line in i.invoice_line_ids])).action_switch_invoice_into_refund_credit_note()
+                [line.quantity <= 0.0 for line in i.invoice_line_ids])).action_switch_move_type()
         return invoices
 
     def action_preview_sale_order(self):
@@ -202,17 +234,14 @@ class SaleOrder(models.Model):
             if prefix:
                 name = prefix + ": " + self.name
             plan = self.env['account.analytic.plan'].sudo().search([
-                ('company_id', '=', False)
             ], limit=1)
             if not plan:
                 plan = self.env['account.analytic.plan'].sudo().create({
                     'name': 'Default',
-                    'company_id': False,
                 })
             return {
                 'name': name,
                 'code': self.client_order_ref,
-                'company_id': False,
                 'plan_id': plan.id,
                 'partner_id': self.partner_id.id,
             }
