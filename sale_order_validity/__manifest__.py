92f76d4d372a0ddca3175d260962ea51861e5c55
Lara <lav@adhoc.com.ar>
2024-01-16 18:08:30 +0000

    [FIX] sale_order_validity: fix in sale_order_view
    closes ingadhoc/sale#769
    
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>

--- sale_order_validity/__manifest__.py
+++ sale_order_validity/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Sale Order Validity',
-    'version': "16.0.1.2.0",
+    'version': "16.0.1.3.0",
     'category': 'Sales & Purchases',
     'sequence': 14,
     'author': 'ADHOC SA',
