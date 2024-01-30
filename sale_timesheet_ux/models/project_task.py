541daecc646a7e095ba1c876e58aa81ed00c3a9c
Alexis <loa@adhoc.com.ar>
2024-01-30 10:53:16 -0300

    [MIG] sale_timesheet_ux: Migration to 17.0

--- sale_timesheet_ux/models/project_task.py
+++ sale_timesheet_ux/models/project_task.py
@@ -13,24 +13,15 @@ class ProjectTask(models.Model):
             recs = recs.filtered(lambda x: not x.is_so_line_edited)
         return recs
 
-    def _compute_sale_order_id(self):
-        # Store allow_billable task's state to mantain the relation
-        # between the task with the SO that originated it when allow_billable is changed in project
-        billable_condition = {x: x.allow_billable for x in self}
-        self.allow_billable = True
-        super()._compute_sale_order_id()
-        for k, v in billable_condition.items():
-            k.allow_billable = v
-
+    def _compute_partner_id(self):
+        for task in self:
+            task_partner_id = task.partner_id or task.project_id.partner_id or task.sale_order_id.partner_id
+            super()._compute_partner_id()
+            task.partner_id = task_partner_id
+    
     def _compute_sale_line(self):
-        # Override this method to combine the behavior between sale_project and sale_timesheet
-        # so the change of value of "allow_billable" to True doesn't unlink the sale_line_id of the task 
-        # with the SO that originated it 
-        billable_tasks = self.filtered('allow_billable')
-        for task in billable_tasks:
-            if not task.sale_line_id:
-                # if the display_project_id is set then it means the task is classic task or a subtask with another project than its parent.
-                task.sale_line_id = task.display_project_id.sale_line_id or task.parent_id.sale_line_id or task.project_id.sale_line_id or task.milestone_id.sale_line_id
-            # check sale_line_id and customer are coherent
-            if task.sale_line_id.order_partner_id.commercial_partner_id != task.partner_id.commercial_partner_id:
-                task.sale_line_id = False
+        for task in self:
+            sale_line = task.sale_line_id or task.parent_id.sale_line_id or task.project_id.sale_line_id or task.milestone_id.sale_line_id
+            super()._compute_sale_line()
+            if not task.sale_line_id or task.sale_line_id != sale_line:
+                task.sale_line_id = sale_line
