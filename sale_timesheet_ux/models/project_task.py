08c8507f5bbf2741ff617a3dd17ee26f25c3d95b
Alexis <loa@adhoc.com.ar>
2024-01-30 10:53:16 -0300

    [MIG] sale_timesheet_ux: Migration to 17.0

--- sale_timesheet_ux/models/project_task.py
+++ sale_timesheet_ux/models/project_task.py
@@ -12,3 +12,16 @@ class ProjectTask(models.Model):
         if self._context.get('write_allow_billiable'):
             recs = recs.filtered(lambda x: not x.is_so_line_edited)
         return recs
+
+    def _compute_partner_id(self):
+        for task in self:
+            task_partner_id = task.partner_id or task.project_id.partner_id or task.sale_order_id.partner_id
+            super()._compute_partner_id()
+            task.partner_id = task_partner_id
+    
+    def _compute_sale_line(self):
+        for task in self:
+            sale_line = task.sale_line_id or task.parent_id.sale_line_id or task.project_id.sale_line_id or task.milestone_id.sale_line_id
+            super()._compute_sale_line()
+            if not task.sale_line_id or task.sale_line_id != sale_line:
+                task.sale_line_id = sale_line
