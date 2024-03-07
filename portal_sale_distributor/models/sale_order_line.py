33b7cbfc9a17ab8ba7ae3bf81e3c84b20119fa92
docker-odoo <lef@adhoc.com.ar>
2024-03-07 17:06:26 +0000

    [IMP] portal_sale_distributor: sudo permission is added to update prices
    closes ingadhoc/sale#795
    
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>

--- portal_sale_distributor/models/sale_order_line.py
+++ portal_sale_distributor/models/sale_order_line.py
@@ -2,8 +2,12 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import ir_action_act_window
-from . import mail_thread
-from . import product_product
-from . import sale_order
-from . import sale_order_line
+from odoo import models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    def _compute_purchase_price(self):
+        self = self.sudo()
+        super()._compute_purchase_price()
