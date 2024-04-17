2f3a007dcc2d8fb70ee37edb92e0b343be61afd7
Lara <lav@adhoc.com.ar>
2024-04-17 16:53:34 -0300

    [FIX] sale_ux: changes in compute price unit

--- sale_ux/models/product_product.py
+++ sale_ux/models/product_product.py
@@ -8,16 +8,18 @@ class ProductProduct(models.Model):
     _inherit = "product.product"
 
     @api.model
-    def _get_tax_included_unit_price(
-            self, company, currency, document_date, document_type,
-            is_refund_document=False, product_uom=None, product_currency=None,
-            product_price_unit=None, product_taxes=None, fiscal_position=None):
+    def _get_tax_included_unit_price_from_price(
+        self, product_price_unit, currency, product_taxes,
+        fiscal_position=None,
+        product_taxes_after_fp=None,
+        is_refund_document=False,
+    ):
         """Modificamos para que como se obtienen los precios no tenga en cuenta en nada la fiscal position.
-        Sin este cambio, en una venta con posicion fiscal que reemplace IVA 21 a exento (o no gravado, si se recalcula
+        Sin este cambio, en una venta con posicion fiscal que reemplace IVA 21 a exento (o no gravado), si se recalcula
         el precio (cambiando cantidad o agregando producto) y el impuesto es con opcion "incluido", el precio que se
-        usa es descontando el impuesto original en vezl del precio final
+        usa es descontando el impuesto original en vez del precio final
         """
-        return super()._get_tax_included_unit_price(
-            company, currency, document_date, document_type,
-            is_refund_document=is_refund_document, product_uom=product_uom, product_currency=product_currency,
-            product_price_unit=product_price_unit, product_taxes=product_taxes, fiscal_position=False)
+        return super()._get_tax_included_unit_price_from_price(
+            product_price_unit, currency, product_taxes, product_taxes_after_fp=product_taxes_after_fp,
+            is_refund_document=is_refund_document,
+            fiscal_position=False)
