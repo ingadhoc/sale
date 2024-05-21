9ff5eeb1717f0c9ea83bd8dd0abef5d77054db05
mav-adhoc <mav@adhoc.com.ar>
2024-05-21 15:39:58 -0300

    [MIG] sale_quotation_products: Migration to 17.0

--- sale_quotation_products/__manifest__.py
+++ sale_quotation_products/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Sale Quotation Products',
-    'version': "16.0.1.0.0",
+    'version': "17.0.1.0.0",
     'category': 'Sales Management',
     'sequence': 14,
     'summary': '',
@@ -36,7 +36,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
