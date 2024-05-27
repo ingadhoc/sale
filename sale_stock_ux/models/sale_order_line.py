c484acafccd31f63cdc30c4ec02aac4f38e6a7af
Juan Ignacio Carreras <jc@adhoc.com.ar>
2024-05-27 18:52:52 +0000

    [IMP]sale_stock_ux: changes with cancel remaining
    closes ingadhoc/sale#835
    
    Related: ingadhoc/stock#486
    Signed-off-by: Matias Velazquez <mav@adhoc.com.ar>

--- sale_stock_ux/models/sale_order_line.py
+++ sale_stock_ux/models/sale_order_line.py
@@ -104,22 +104,13 @@ class SaleOrderLine(models.Model):
             #         'You can not cancel remianing qty to deliver because '
             #         'there are more product invoiced than the delivered. '
             #         'You should correct invoice or ask for a refund'))
-            rec.with_context(
-                bypass_protecion=True).product_uom_qty = rec.qty_delivered
-            to_cancel_moves = rec.move_ids.filtered(
-                lambda x: x.state not in ['done', 'cancel'])
-            to_cancel_moves._cancel_quantity()
+            rec.product_uom_qty = rec.qty_delivered
             rec.order_id.message_post(
                 body=_(
                     'Cancel remaining call for line "%s" (id %s), line '
                     'qty updated from %s to %s') % (
                         rec.name, rec.id,
                         old_product_uom_qty, rec.product_uom_qty))
-
-    def _get_protected_fields(self):
-        if self._context.get('bypass_protecion'):
-            return []
-        return super()._get_protected_fields()
 
     @api.onchange('product_uom_qty')
     def _onchange_product_uom_qty(self):
