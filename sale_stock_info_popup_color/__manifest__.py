247099c5b4413785c445637b1033049e22113f31
mav-adhoc <mav@adhoc.com.ar>
2024-06-12 13:46:20 -0300

    [MIG] sale_stock_info_popup_color: Migration to 17.0

--- sale_stock_info_popup_color/__manifest__.py
+++ sale_stock_info_popup_color/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Sale Stock Info Pop-up Color',
-    'version': "16.0.1.2.0",
+    'version': "17.0.1.0.0",
     'category': 'Sales',
     'sequence': 14,
     'summary': '',
@@ -17,15 +17,11 @@
     'assets': {
         'web.assets_backend': [
             'sale_stock_info_popup_color/static/src/**/*',
-            ('remove', 'sale_stock_info_popup_color/static/src/legacy/**/*'),
         ],
-        "web.assets_backend_legacy_lazy": [
-            'sale_stock_info_popup_color/static/src/legacy/**/*',
-        ]
     },
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
