0c5378229869a2dd22ef518772d9ad239b6bc52a
Franco Leyes <lef@adhoc.com.ar>
2024-08-06 12:58:37 +0000

    [FIX] sale_three_discounts: when updating prices put the discounts back on
    X-original-commit: 66fea5d30afdee96802a744fa91bae85a38fcb6b

--- sale_three_discounts/models/sale_order.py
+++ sale_three_discounts/models/sale_order.py
@@ -9,4 +9,24 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     def _recompute_prices(self):
+<<<<<<< HEAD
         super(SaleOrder, self.with_context(recompute_prices=True))._recompute_prices()
+||||||| parent of 48075273 (temp)
+        if self.pricelist_id.discount_policy == 'with_discount':
+            discount1 = {x: x.discount1 for x in self.order_line}
+            super()._recompute_prices()
+            for k, v in discount1.items():
+                k.discount1 = v
+        else:
+            super()._recompute_prices()
+=======
+        if self.pricelist_id.discount_policy == 'with_discount':
+            discounts = {x: (x.discount1, x.discount2, x.discount3) for x in self.order_line}
+            super()._recompute_prices()
+            for line, (disc1, disc2, disc3) in discounts.items():
+                line.discount1 = disc1
+                line.discount2 = disc2
+                line.discount3 = disc3
+        else:
+            super()._recompute_prices()
+>>>>>>> 48075273 (temp)
