e8137f716435a931db9b5c8b8c8015cf28176a4b
Franco Leyes <lef@adhoc.com.ar>
2024-08-20 15:21:25 -0300

    Revert "[IMP] sale_three_discounts: add integration test"
    This reverts commit bf34f398072c7e3c0bca3e8eb057e787688c564d.

--- sale_three_discounts/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import test_sale_three_discounts
