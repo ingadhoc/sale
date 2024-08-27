9392dd1ebd81c0e2f874006060bf33752a93ecff
Matias Peralta <matias_peralta1@hotmail.com>
2024-08-27 16:43:15 -0300

    [FIX] portal_sale_distributor: calendar error fixed

--- portal_sale_distributor/__manifest__.py
+++ portal_sale_distributor/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Portal Distributor Sale',
-    'version': "17.0.1.2.0",
+    'version': "17.0.1.3.0",
     'category': 'Tools',
     'complexity': 'easy',
     'author': 'ADHOC SA, Odoo Community Association (OCA)',
