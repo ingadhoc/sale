be1321588c4e0e3da71841fab6ae5b3e294468dd
Franco Leyes <lef@adhoc.com.ar>
2024-09-02 19:28:36 +0000

    [IMP] sale_order_type_invoice_policy: to be able to pay invoice with credit note
    closes ingadhoc/sale#918
    
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>

--- sale_order_type_invoice_policy/models/stock_picking.py
+++ sale_order_type_invoice_policy/models/stock_picking.py
@@ -40,12 +40,11 @@ class StockPicking(models.Model):
         return super(StockPicking, self).action_assign()
 
     def _check_sale_paid(self):
-        precision = self.env['decimal.precision'].precision_get(
-            'Product Unit of Measure')
-        invoice_status = self.sale_id.mapped(
-            'order_line.invoice_lines.move_id').filtered(lambda x: x.move_type == 'out_invoice').mapped('payment_state')
-        if (set(invoice_status) - set(['paid', 'in_payment'])) or any(
-                not float_is_zero(line.qty_to_invoice, precision_digits=precision)
-                for line in self.sale_id.order_line):
+        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+        invoice_status = self.sale_id.mapped('order_line.invoice_lines.move_id').filtered(lambda x: x.move_type == 'out_invoice' and x.state != 'cancel').mapped('payment_state')
+        paid_status = ['paid', 'in_payment', 'reversed']
+        if (set(invoice_status) - set(paid_status)) or any(
+            not float_is_zero(line.qty_to_invoice, precision_digits=precision) for line in self.sale_id.order_line
+        ):
             return False
         return True
