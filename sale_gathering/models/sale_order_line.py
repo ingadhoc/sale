899e42f9c65dbe64ab7024153d75eafd8892ea98
Franco Leyes <lef@adhoc.com.ar>
2024-09-04 14:29:09 +0000

    [IMP] sale_gathering: group to allow discounts on redeemed products
    closes ingadhoc/sale#922
    
    X-original-commit: 6eea4d85186ff98fd04ec6900909a035978641ea
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>
    Signed-off-by: Franco Leyes <lef@adhoc.com.ar>

--- sale_gathering/models/sale_order_line.py
+++ sale_gathering/models/sale_order_line.py
@@ -51,11 +51,26 @@ class SaleOrderLine(models.Model):
 
     def write(self, vals):
         if "discount" in vals:
-            if self.filtered(lambda x: x.order_id.is_gathering and x.order_id.state == 'sale' and x.initial_qty_gathered > 0):
-                raise UserError(_("You cannot modify the discount of the gathering lines once the sale has been confirmed.\n"))
+            gathering_lines = self.filtered(
+                lambda x: x.order_id.is_gathering and x.order_id.state == 'sale'
+            )
+            if gathering_lines:
+                if gathering_lines.filtered(lambda x: x.initial_qty_gathered > 0):
+                    raise UserError(_("You cannot modify the discount of the gathering lines once the sale has been confirmed.\n"))
+                if gathering_lines.filtered(lambda x: x.initial_qty_gathered == 0 and (x.qty_invoiced > 0 or x.qty_delivered > 0)):
+                    raise UserError(_("It is not possible to add discounts once the product has been invoiced or delivered."))
         return super().write(vals)
 
     @api.constrains('discount')
     def _check_discount(self):
-        for rec in self.filtered(lambda x: x.order_id.is_gathering and x.state == 'sale' and x.initial_qty_gathered == 0 and x.discount > 0):
-            raise ValidationError(_("Cannot add discounts to redeemed products."))
+        for rec in self:
+            if rec.order_id.is_gathering and rec.state == 'sale':
+                if rec.is_downpayment and rec.discount > 0:
+                    raise ValidationError(_("Discounts cannot be added to downpayments."))
+
+                if (
+                    not self.env.user.has_group('sale_gathering.group_allow_redeemed_product_discounts')
+                    and rec.initial_qty_gathered == 0
+                    and rec.discount > 0
+                ):
+                    raise ValidationError(_("Cannot add discounts to redeemed products."))
