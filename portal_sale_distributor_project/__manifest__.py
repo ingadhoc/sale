b93f27b40ca8a33103445ceadd6706edeed0498e
docker-odoo <lef@adhoc.com.ar>
2024-10-03 20:15:17 +0000

    [MIG] portal_sale_distributor_project: Migration to 17.0

--- portal_sale_distributor_project/__manifest__.py
+++ portal_sale_distributor_project/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Portal Distributor Project',
-    'version': "16.0.1.0.0",
+    'version': "17.0.1.0.0",
     'category': 'Tools',
     'complexity': 'easy',
     'author': 'ADHOC SA',
@@ -34,6 +34,6 @@
     'data': [
         'security/ir.model.access.csv',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
 }
