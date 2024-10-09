cce72086d5e99537dff6f077e2a2eb445f8665ca
Juan Ignacio Carreras <jc@adhoc.com.ar>
2024-10-09 10:23:56 -0300

    [IMP]sale_timesheet_ux:warning confirmation

--- sale_timesheet_ux/models/project_task.py
+++ sale_timesheet_ux/models/project_task.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class ProjectTask(models.Model):
@@ -25,3 +26,14 @@ class ProjectTask(models.Model):
             super()._compute_sale_line()
             if not task.sale_line_id or task.sale_line_id != sale_line:
                 task.sale_line_id = sale_line
+
+    @api.onchange('sale_line_id')
+    def _onchange_sale_line_id(self):
+        for task in self:
+            if task.sale_line_id:
+                return {
+                    'warning': {
+                        'title': "Confirmación",
+                        'message': "Tenga en cuenta que cambiar la linea de venta de la tarea recomputará las lineas de venta de las lineas de parte de hora NO validadas. ¿Está seguro de que desea cambiarla?",
+                    }
+                }
