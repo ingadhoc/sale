440b1ade638d200dc60d14c8fa44e7b055d525f1
docker-odoo <docker-odoo@example.com>
2024-10-10 15:36:46 +0000

    [MIG] sale_ux: Migration to 18.0

--- sale_ux/__manifest__.py
+++ sale_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Sale UX',
-    'version': "17.0.1.5.0",
+    'version': "18.0.1.0.0",
     'category': 'Sales',
     'sequence': 14,
     'summary': '',
@@ -50,7 +50,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
