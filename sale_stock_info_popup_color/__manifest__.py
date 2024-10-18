a8f7c9606f420a6507927699c275e9ce6b22eecc
docker-odoo <docker-odoo@example.com>
2024-10-18 19:14:54 +0000

    [MIG] sale_stock_info_popup_color: Migration to 18.0

--- sale_stock_info_popup_color/__manifest__.py
+++ sale_stock_info_popup_color/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Sale Stock Info Pop-up Color',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'category': 'Sales',
     'sequence': 14,
     'summary': '',
@@ -21,7 +21,7 @@
     },
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
