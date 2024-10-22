f9fcecdde507eaddff4f9be7a9b7a7c271e3ac75
docker-odoo <docker-odoo@example.com>
2024-10-22 19:17:11 +0000

    [MIG] sale_require_contract: Migration to 18.0

--- sale_require_contract/__manifest__.py
+++ sale_require_contract/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Sale Order Require Contract on Confirmation',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'category': 'Projects & Services',
     'sequence': 14,
     'summary': '',
@@ -38,7 +38,7 @@
         'demo/exception_rule_demo.xml',
         'demo/sale_order_demo.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
