a277c470d2c5a84c07e9d339dac84040b1a0d90d
docker-odoo <docker-odoo@example.com>
2024-10-22 18:33:15 +0000

    [MIG] sale_margin_ux: Migration to 18.0
    closes ingadhoc/sale#985
    
    Signed-off-by: Juan Ignacio Rivero <jr@adhoc.com.ar>

--- sale_margin_ux/__manifest__.py
+++ sale_margin_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Sale Margin Ux',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'category': 'Sales & Purchases',
     'sequence': 14,
     'summary': '',
@@ -37,7 +37,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'application': False,
 }
