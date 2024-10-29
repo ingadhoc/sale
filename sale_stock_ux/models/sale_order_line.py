e22c3a342d183358a024cccb5b3f010437e72f84
mav-adhoc <mav@adhoc.com.ar>
2024-10-29 17:54:30 +0000

    [FIX]sale_Stock_ux: revert compute_invoice_status
    X-original-commit: 2b325073fb767a7606a960e28f8c14e88a5b36d2

--- sale_stock_ux/models/sale_order_line.py
+++ sale_stock_ux/models/sale_order_line.py
@@ -224,3 +224,26 @@ class SaleOrderLine(models.Model):
                 line.qty_to_invoice = (
                     line.product_uom_qty - line.quantity_returned -
                     line.qty_invoiced)
+<<<<<<< HEAD
+||||||| parent of c7c8e6b7 (temp)
+
+    @api.depends('order_id.force_invoiced_status', 'state', 'product_uom_qty', 'qty_delivered', 'qty_to_invoice', 'qty_invoiced')
+    def _compute_invoice_status(self):
+        super()._compute_invoice_status()
+        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+        for line in self:
+            if float_compare(line.qty_invoiced, (line.product_uom_qty - line.quantity_returned),
+                            precision_digits=precision) >= 0:
+                line.invoice_status = 'invoiced'
+=======
+
+    # Queda comentado ya que habría que incorporarlo a futuro en v17, peor en v16 rompe.
+    # @api.depends('order_id.force_invoiced_status', 'state', 'product_uom_qty', 'qty_delivered', 'qty_to_invoice', 'qty_invoiced')
+    # def _compute_invoice_status(self):
+    #     super()._compute_invoice_status()
+    #     precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+    #     for line in self:
+    #         if float_compare(line.qty_invoiced, (line.product_uom_qty - line.quantity_returned),
+    #                         precision_digits=precision) >= 0:
+    #             line.invoice_status = 'invoiced'
+>>>>>>> c7c8e6b7 (temp)
