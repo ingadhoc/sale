e79031bc67141a0930dbd098edb5d08ac9a9aad9
docker-odoo <docker-odoo@example.com>
2024-10-30 19:12:10 +0000

    [MIG] sale_gathering_index: Migration to 18.0

--- sale_gathering_index/__manifest__.py
+++ sale_gathering_index/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Sale Gathering Index',
-    'version': "17.0.1.1.0",
+    'version': "18.0.1.0.0",
     'category': 'Sales & Stock',
     'sequence': 14,
     'summary': '',
@@ -37,7 +37,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
--- sale_gathering_index/models/sale_order_line.py
+++ sale_gathering_index/models/sale_order_line.py
@@ -24,42 +24,47 @@ class SaleOrderLine(models.Model):
                 and line.initial_qty_gathered == 0 for line in self
             ):
                 raise UserError(_("You can't modify the quantity of an added product. Please add a new line."))
-        return super(SaleOrderLine, self).write(vals)
+        if 'name' in vals:
+            if any(
+                line.order_id.is_gathering
+                and line.order_id.state == 'sale'
+                and line.display_type not in ['line_section', 'line_note']
+                and not line.is_downpayment
+                for line in self
+            ):
+                raise UserError(_("You can't modify the description."))
+        return super().write(vals)
 
     def _compute_price_unit(self):
         super()._compute_price_unit()
         for rec in self.filtered(lambda x: x.order_id.is_gathering and x.order_id.state == 'sale' and x.initial_qty_gathered == 0):
-            """
-            Evaluamos si es NewId porque al agregar una nueva línea a la orden de venta (sin guardar) el index
-            lo toma como 0 al no poner _origin ya que en esa order new aún no ha sido calculado el índice.
-            Utilizamos el origin de forma segura ya que filtramos las órdenes de venta que están en estado 'sale'
-            por lo tanto ya cuando busque el origin va a existir en la base de datos y no va a haber error.
-            Otra forma que funcionó es haciendo depends de order_id.index pero solamente entraba a compute_name
-            y para que ingrese a este método (no entiendo por qué no lo hacía) tenía que poner el price_unit
-            como recursive=True, pero este último approach ingresaba varias veces a computar y tiene menor performance.
-            """
-            if isinstance(rec.order_id.id, models.NewId):
-                index = rec.order_id._origin.index
+            if not rec.product_uom or not rec.product_id:
+                price_unit = 0.0
             else:
-                index = rec.order_id.index
-            rec.price_unit = rec.price_unit / (index + 1)
+                line = rec.with_company(rec.company_id)
+                price = line._get_display_price()
+                price_unit = line.product_id._get_tax_included_unit_price_from_price(
+                    price,
+                    product_taxes=line.product_id.taxes_id.filtered(
+                        lambda tax: tax.company_id == line.env.company
+                    ),
+                    fiscal_position=line.order_id.fiscal_position_id,
+                )
+            rec.price_unit = price_unit / (rec.order_id.index + 1)
 
     def _compute_name(self):
         super()._compute_name()
         for line in self.filtered(lambda x: x.order_id.is_gathering and x.order_id.state == 'sale' and x.initial_qty_gathered == 0 and not x.display_type and not x.is_downpayment and x.name):
-            price = line.with_company(line.company_id)._get_display_price()
-            line_price = line.product_id._get_tax_included_unit_price(
-                line.company_id,
-                line.order_id.currency_id,
-                line.order_id.date_order,
-                'sale',
+            line = line.with_company(line.company_id)
+            price = line._get_display_price()
+            price_unit = line.product_id._get_tax_included_unit_price_from_price(
+                price,
+                product_taxes=line.product_id.taxes_id.filtered(
+                    lambda tax: tax.company_id == line.env.company
+                ),
                 fiscal_position=line.order_id.fiscal_position_id,
-                product_price_unit=price,
-                product_currency=line.currency_id
             )
-            # misma nota que en método _compute_price_unit
-            if isinstance(line.order_id.id, models.NewId):
-                index = line.order_id._origin.index
-            else:
-                index = line.order_id.index
-            line.name += "\n($%s)\n(%s%%)" % (line_price, round(index*100, 2))
+            coefficient = 1 / (line.order_id.index + 1)
+            line.name += _(
+                "\n(Current price: $%s | Coef: %s%%)"
+            ) % (price_unit, round(coefficient, 4))
