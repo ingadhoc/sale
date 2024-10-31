563f4590b49c06e305de248f7e34856923580c84
docker-odoo <docker-odoo@example.com>
2024-10-31 15:00:57 +0000

    [MIG] sale_barcode: Migration to 18.0

--- sale_barcode/__manifest__.py
+++ sale_barcode/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Sale Barcode',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'category': 'Sales',
     'sequence': 14,
     'summary': '',
@@ -38,7 +38,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
