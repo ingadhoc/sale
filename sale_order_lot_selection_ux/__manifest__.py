5b2f30580222ecd4fd5f6c34b0e4f63501dede80
docker-odoo <docker-odoo@example.com>
2024-11-14 18:10:29 +0000

    [MIG] sale_order_lot_selection_ux: Migration to 18.0

--- sale_order_lot_selection_ux/__manifest__.py
+++ sale_order_lot_selection_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Sale Order Lot Selection UX',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'category': 'Sale',
     'sequence': 14,
     'author': 'ADHOC SA',
@@ -32,7 +32,7 @@
         'views/sale_order_views.xml',
         'views/stock_production_lot.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
