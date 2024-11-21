78c588c752ba7a8ef1d38d1c07ba524f9aae9a2b
Lara <lav@adhoc.com.ar>
2024-11-21 09:11:37 -0300

    [IMP] portal_sale_distributor: add access right for sale_exception

--- portal_sale_distributor/__manifest__.py
+++ portal_sale_distributor/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Portal Distributor Sale',
-    'version': "18.0.1.0.0",
+    'version': "18.0.1.1.0",
     'category': 'Tools',
     'complexity': 'easy',
     'author': 'ADHOC SA, Odoo Community Association (OCA)',
