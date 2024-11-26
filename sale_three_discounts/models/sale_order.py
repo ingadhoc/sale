a92afcc27f4fafbd78d7903bd3761ef53489a198
Franco Leyes <lef@adhoc.com.ar>
2024-11-26 14:33:41 +0000

    [FIX] sale_three_discounts: recalculation when changing pricelist
    X-original-commit: 09162b86b60422881fa8ee8c252767f25b3c1faf

--- sale_three_discounts/models/sale_order.py
+++ sale_three_discounts/models/sale_order.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models
+from odoo import models, api
 
 
 class SaleOrder(models.Model):
@@ -10,3 +10,12 @@ class SaleOrder(models.Model):
 
     def _recompute_prices(self):
         super(SaleOrder, self.with_context(recompute_prices=True))._recompute_prices()
+
+    @api.onchange('pricelist_id')
+    def _onchange_pricelist(self):
+        self.order_line.write({
+            'discount1': 0,
+            'discount2': 0,
+            'discount3': 0
+        })
+        self._recompute_prices()
