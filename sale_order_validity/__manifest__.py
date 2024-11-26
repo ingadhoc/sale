2c914cffffc6ca89e5b53ea4e8e234947c7aef6e
Lara <lav@adhoc.com.ar>
2024-11-26 16:53:16 -0300

    [FIX] sale_order_validity: fix in sale order form view

--- sale_order_validity/__manifest__.py
+++ sale_order_validity/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Sale Order Validity',
-    'version': "18.0.1.0.0",
+    'version': "18.0.1.1.0",
     'category': 'Sales & Purchases',
     'sequence': 14,
     'author': 'ADHOC SA',
