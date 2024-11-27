379901100d74799110d36ec213248ef3a4cc6350
Matias Peralta <matias_peralta1@hotmail.com>
2024-11-27 13:34:55 -0300

    [FIX] sale_stock_ux: revert compute_invoice_status

--- sale_stock_ux/models/sale_order_line.py
+++ sale_stock_ux/models/sale_order_line.py
@@ -239,14 +239,15 @@ class SaleOrderLine(models.Model):
         super()._compute_invoice_status()
         precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
         for line in self:
-            if not float_is_zero(line.qty_to_invoice, precision_digits=precision):
-                line.invoice_status = 'to invoice'
-            elif line.state == 'sale' and line.product_id.invoice_policy == 'order' and\
-                    line.product_uom_qty >= 0.0 and\
-                    float_compare(line.qty_delivered, line.product_uom_qty, precision_digits=precision) == 1:
-                line.invoice_status = 'upselling'
-            elif float_compare(line.qty_invoiced, (line.product_uom_qty - line.quantity_returned),
-                            precision_digits=precision) >= 0:
-                line.invoice_status = 'invoiced'
-            else:
-                line.invoice_status = 'no'
+            if not line.order_id.force_invoiced_status:
+                if not float_is_zero(line.qty_to_invoice, precision_digits=precision):
+                    line.invoice_status = 'to invoice'
+                elif line.state == 'sale' and line.product_id.invoice_policy == 'order' and\
+                        line.product_uom_qty >= 0.0 and\
+                        float_compare(line.qty_delivered, line.product_uom_qty, precision_digits=precision) == 1:
+                    line.invoice_status = 'upselling'
+                elif float_compare(line.qty_invoiced, (line.product_uom_qty - line.quantity_returned),
+                                precision_digits=precision) >= 0:
+                    line.invoice_status = 'invoiced'
+                else:
+                    line.invoice_status = 'no'
