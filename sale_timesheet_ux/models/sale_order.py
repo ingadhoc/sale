a5bbc4ea8f533a72d31071db95c3b6fe47408078
Franco Leyes <lef@adhoc.com.ar>
2025-01-08 18:20:47 +0000

    [REF] sale_timesheet_ux: changes due to runbot warning
    closes ingadhoc/sale#1101
    
    Signed-off-by: Juan Carreras <jc@adhoc.com.ar>

--- sale_timesheet_ux/models/sale_order.py
+++ sale_timesheet_ux/models/sale_order.py
@@ -9,5 +9,5 @@ class SaleOrder(models.Model):
     def _compute_tasks_ids(self):
         super()._compute_tasks_ids()
         for order in self:
-            order.tasks_ids = self.env['project.task'].search(['&', ('project_id', '!=', 'False'), '|','|', ('sale_line_id', 'in', order.order_line.ids), ('sale_order_id', '=', order.id), ('order_id', '=', order.id)])
+            order.tasks_ids = self.env['project.task'].search(['&', ('project_id', '!=', 'False'), '|','|', ('sale_line_id', 'in', order.order_line.ids), ('sale_order_id', 'in', order.ids), ('order_id', 'in', order.ids)])
             order.tasks_count = len(order.tasks_ids)
