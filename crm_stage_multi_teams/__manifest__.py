c329a810b604c036521a8e660fafc1af8d54f77b
Franco Leyes <lef@adhoc.com.ar>
2025-01-14 15:35:18 +0000

    [FIX] crm_teams_ux: fix domain of users and stages in leads
    X-original-commit: 0c4177302d2f7f5334d5177b7c8c8ca768678250

--- crm_stage_multi_teams/__manifest__.py
+++ crm_stage_multi_teams/__manifest__.py
@@ -1,8 +1,16 @@
 # © 2016 ADHOC SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
+<<<<<<< HEAD:crm_stage_multi_teams/__manifest__.py
     'name': 'CRM Stage Multi Teams',
     'version': "18.0.1.0.0",
+||||||| parent of 13273116 (temp):crm_teams_ux/__manifest__.py
+    'name': 'CRM Teams UX',
+    'version': "17.0.1.0.0",
+=======
+    'name': 'CRM Teams UX',
+    'version': "17.0.1.1.0",
+>>>>>>> 13273116 (temp):crm_teams_ux/__manifest__.py
     'sequence': 14,
     'summary': '',
     'author': 'ADHOC SA',
