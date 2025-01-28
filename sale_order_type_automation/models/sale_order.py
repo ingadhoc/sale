224a545c5a4ad95bd239f6e3a4f4fab737e7a34d
Franco Leyes <lef@adhoc.com.ar>
2025-01-28 11:05:35 -0300

    [REF] sale_order_type_automation: refactoring to make inheritable

--- sale_order_type_automation/models/sale_order.py
+++ sale_order_type_automation/models/sale_order.py
@@ -48,61 +48,57 @@ class SaleOrder(models.Model):
                     invoices.message_post(body=message)
                     rec.message_post(body=message)
 
-    def run_picking_atomation(self):
+    def run_picking_automation(self):
         # If there products are the type 'service' equals the
         #  delivered qyt to order qty for this sale order line
-        for order_lines in self.mapped('order_line').filtered(
-            lambda x: x.order_id.type_id.picking_atomation !=
-            'none' and x.product_id.type ==
-            'service' and x.product_id.service_type ==
-                'manual' and x.product_id.expense_policy == 'no'):
-            order_lines.qty_delivered = order_lines.product_uom_qty
+        for order_line in self.mapped('order_line').filtered(
+            lambda x: x.order_id.type_id.picking_atomation != 'none'
+            and x.product_id.type == 'service'
+            and x.product_id.service_type == 'manual'
+            and x.product_id.expense_policy == 'no'
+        ):
+            order_line.qty_delivered = order_line.product_uom_qty
         for rec in self.filtered(
-                lambda x: x.type_id.picking_atomation != 'none' and
-                x.procurement_group_id):
-            jit_installed = self.env['ir.module.module'].search(
-                [('name', '=', 'procurement_jit'),
-                    ('state', '=', 'installed')], limit=1)
-            stock_voucher_installed = self.env['ir.module.module'].search(
-                [('name', '=', 'stock_voucher'),
-                    ('state', '=', 'installed')], limit=1)
-            # we add invalidate because on boggio we have add an option
-            # for tracking_disable and with that setup pickings where not seen
-            # rec.invalidate_cache()
+            lambda x: x.type_id.picking_atomation != 'none'
+            and x.procurement_group_id
+        ):
             pickings = rec.picking_ids.filtered(
-                lambda x: x.state not in ('done', 'cancel'))
-            if not jit_installed:
-                pickings.action_assign()
-            # ordenamos primeros los pickings asignados y luego el resto
-            assigned_pickings = pickings.filtered(
-                lambda x: x.state == 'assigned')
-            pickings = assigned_pickings + (pickings - assigned_pickings)
-            for pick in pickings:
-                if rec.type_id.picking_atomation == 'validate':
-                    # this method already call action_assign
-                    pick.new_force_availability()
-                elif rec.type_id.picking_atomation == 'validate_no_force':
-                    products = []
-                    for move in pick.mapped('move_ids'):
-                        if move.state != 'assigned':
-                            products.append(move.product_id)
-                    if products:
-                        raise UserError(_(
-                            'The following products are not available, we '
-                            'suggest to check stock or to use a sale type that'
-                            'force availability.\nProducts:\n* %s\n'
-                        ) % ('\n * '.join(x.name for x in products)))
-                    for op in pick.mapped('move_line_ids'):
-                        op.quantity = op.quantity_product_uom
-                if not stock_voucher_installed:
-                    pick.button_validate()
+                lambda x: x.state not in ('done', 'cancel')
+            )
+            pickings.action_assign()
+            pickings = pickings.sorted(
+                key=lambda x: (x.state != 'assigned', x.id)
+            )
+            self._process_pickings(pickings, rec)
+        return True
+
+    def _process_pickings(self, pickings, rec):
+        products = []
+        for pick in pickings:
+            if rec.type_id.picking_atomation == 'validate':
+                pick.new_force_availability()
+            elif rec.type_id.picking_atomation == 'validate_no_force':
+                products.extend(
+                    move.product_id for move in pick.move_ids if move.state != 'assigned'
+                )
+                if products:
+                    raise UserError(_(
+                        'The following products are not available, we '
+                        'suggest to check stock or to use a sale type that'
+                        'force availability.\nProducts:\n* %s\n'
+                    ) % '\n* '.join(x.name for x in products))
+
+                for op in pick.move_line_ids:
+                    op.quantity = op.quantity_product_uom
+
+            pick.button_validate()
 
     def action_confirm(self):
         res = super().action_confirm()
         # we use this because compatibility with sale exception module
         if isinstance(res, bool) and res:
             # because it's needed to return actions if exists
-            self.run_picking_atomation()
+            res = self.run_picking_automation()
             self.sudo().run_invoicing_atomation()
             if self.type_id.set_done_on_confirmation:
                 self.action_lock()
