074ba8e0ca7699e118e234247bf2fdbe472769ae
Matias Peralta <matias_peralta1@hotmail.com>
2025-02-06 10:58:44 -0300

    [REF] pos_ux: intermediary account is required

--- pos_ux/__manifest__.py
+++ pos_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Point of Sale UX',
-    'version': "17.0.1.1.0",
+    'version': "17.0.1.2.0",
     'category': 'Point of Sale',
     'description': """
 This module extend functionality of point of sale .
