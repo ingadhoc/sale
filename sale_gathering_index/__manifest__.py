fabd83ef5db127de5f1c90c4c89925c0eeda4e92
Franco Leyes <lef@adhoc.com.ar>
2025-02-12 13:11:45 +0000

    [FIX] sale_gathering,sale_gathering_index: correct invoice printing
    X-original-commit: 910df7c87a5b02a9d1b9057db57003cdeb113b8c

--- sale_gathering_index/__manifest__.py
+++ sale_gathering_index/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Sale Gathering Index',
-    'version': "17.0.1.2.0",
+    'version': "17.0.1.3.0",
     'category': 'Sales & Stock',
     'sequence': 14,
     'summary': '',
