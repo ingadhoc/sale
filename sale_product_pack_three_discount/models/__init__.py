72fb239931014ac7170ae444add3ad2abce6d409
Matias Peralta <matias_peralta1@hotmail.com>
2025-02-13 17:55:18 +0000

    [ADD] sale_product_pack_three_discounts: new module
    X-original-commit: 4bd52d8ab9aa9ddd3de17a69877defad3d9efb4d

--- sale_product_pack_three_discount/models/__init__.py
+++ sale_product_pack_three_discount/models/__init__.py
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 ##############################################################################
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
 from . import sale_order_line
+||||||| parent of a84fc237 (temp)
+=======
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import sale_order_line
+
+>>>>>>> a84fc237 (temp)
--- sale_product_pack_three_discount/models/sale_order_line.py
+++ sale_product_pack_three_discount/models/sale_order_line.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ##############################################################################
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
@@ -21,3 +22,26 @@ class SaleOrder(models.Model):
             line.discount1 = line.discount
             line.discount2 = 0.0
             line.discount3 = 0.0
+||||||| parent of a84fc237 (temp)
+=======
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order.line"
+
+    def _compute_discount(self):
+        for pack_line in self.filtered("pack_parent_line_id"):
+            context = self._context
+            if context.get('pack_parent_line'):
+                pack_line.discount1 = pack_line._get_pack_line_discount()
+        res = super()._compute_discount()
+        return res
+
+    def expand_pack_line(self, write=False):
+        return super(SaleOrder, self.with_context(pack_parent_line=True, recompute_prices=True)).expand_pack_line(write=write)
+>>>>>>> a84fc237 (temp)
