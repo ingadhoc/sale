1292bdf406a38f44490fcfa7febae3bd4a9a97ad
Matias Peralta <matias_peralta1@hotmail.com>
2025-03-12 15:53:34 -0300

    [IMP] pos_ux: cannot open new session whether some payment method do not have intermediary account

--- pos_ux/models/pos_config.py
+++ pos_ux/models/pos_config.py
@@ -1,4 +1,6 @@
 from odoo import models, fields, api
+from odoo.tools.translate import _
+from odoo.exceptions import UserError
 
 
 class PosConfig(models.Model):
@@ -8,3 +10,12 @@ class PosConfig(models.Model):
     billing_behavior = fields.Selection([('on_demand','Invoice on demand'), ('invoice_by_default', 'By default invoice'),
                                          ('invoice_required', 'allways Invoice')], default='on_demand')
     block_invoice_download = fields.Boolean()
+
+    def open_ui(self):
+        for config in self:
+            invalid_payment_methods = config.payment_method_ids.filtered(
+            lambda method: not method.split_transactions and not method.receivable_account_id)
+            if invalid_payment_methods:
+                payment_method_names = ', '.join(invalid_payment_methods.mapped('name'))
+                raise UserError(_("No se puede completar la operación: falta definir cuenta intermediaria en los siguientes métodos de pago: %s") % payment_method_names)
+        return super().open_ui()
