880f325d2142b4e72b75c1f08cc35db010657460
Franco Leyes <lef@adhoc.com.ar>
2025-03-12 15:56:04 +0000

    [IMP] sale_gathering_index: add coef in form view
    closes ingadhoc/sale#1162
    
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>

--- sale_gathering_index/__manifest__.py
+++ sale_gathering_index/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Sale Gathering Index',
-    'version': "16.0.1.3.0",
+    'version': "16.0.1.4.0",
     'category': 'Sales & Stock',
     'sequence': 14,
     'summary': '',
