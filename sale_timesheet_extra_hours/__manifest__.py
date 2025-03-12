7b4ac4ca16dad97cf3d61e86936c0084dc5afc08
mav-adhoc <mav@adhoc.com.ar>
2025-03-12 15:31:08 -0300

    [IMP]sale_timesheet_extra_hours: keep same cost for imputed lines when changing the fare on the project

--- sale_timesheet_extra_hours/__manifest__.py
+++ sale_timesheet_extra_hours/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Sales Timesheet Extra Hours",
     "summary": "",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "website": "www.adhoc.com.ar",
     "license": "AGPL-3",
     "author": "ADHOC SA",
