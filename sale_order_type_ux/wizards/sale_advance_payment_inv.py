c50263178f47ca07b720bd67cea0cbb83ee69597
Rocío Vega <rov@adhoc.com.ar>
2025-04-07 17:14:56 +0000

    Cherry pick of b5bdf1c8f8e58a86fda74bbd39efe3a349b7ab1f failed
    stdout:
    
    stderr:
    18:24:18.768614 git.c:444               trace: built-in: git cherry-pick b5bdf1c8f8e58a86fda74bbd39efe3a349b7ab1f
    error: Cherry-picking is not possible because you have unmerged files.
    hint: Fix them up in the work tree, and then use 'git add/rm <file>'
    hint: as appropriate to mark resolution and make a commit.
    fatal: cherry-pick failed
    ----------
    status:

--- sale_order_type_ux/wizards/sale_advance_payment_inv.py
+++ sale_order_type_ux/wizards/sale_advance_payment_inv.py
@@ -30,3 +30,9 @@ class SaleAdvancePaymentInv(models.TransientModel):
                         line[2]["tax_ids"] = [(6, 0, tax_ids)]
 
         return res
+
+    def _prepare_down_payment_product_values(self):
+        res = super()._prepare_down_payment_product_values()
+        if res["company_id"]:
+            res["company_id"] = False
+        return res
