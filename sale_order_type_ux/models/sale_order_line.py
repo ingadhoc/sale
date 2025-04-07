e1538838c1e95819ecf1a340be23b0fea1ae2938
Rocío Vega <rov@adhoc.com.ar>
2025-04-07 17:14:56 +0000

    [FIX] sale_order_type_ux: Disable check_company on tax_id to allow multi-company advance invoices
    In certain cases, a sales order may belong to one company while its related advance invoice
    is created in another. When this happens, assigning taxes to the invoice would raise a
    ValidationError due to mismatched company IDs on the Many2many tax_id field.
    
    To prevent this, we explicitly disable check_company on the tax_id field. This allows taxes
    from a different company to be assigned when necessary, particularly in multi-company flows
    involving down payments or prepayments.
    
    This change is intentional and limited in scope. Company consistency should still be ensured
    at the business logic level where appropriate.
    
    closes ingadhoc/sale#1183
    
    Signed-off-by: Camila Vives <cav@adhoc.com.ar>

--- sale_order_type_ux/models/sale_order_line.py
+++ sale_order_type_ux/models/sale_order_line.py
@@ -2,11 +2,17 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, api
+from odoo import models, api, fields
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
+
+    # Quitamos el check_company ya que no permitira 
+    # realizar un cambio de compañia entre la venta y su factura de anticipo
+    tax_id = fields.Many2many(
+        check_company=False
+    )
 
     def _prepare_invoice_line(self, **optional_values):
         """
