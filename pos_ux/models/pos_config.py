a9dbfe9430030ff4d87aa774d3644cb9c28a88aa
Matias Peralta <matias_peralta1@hotmail.com>
2025-04-15 20:02:05 +0000

    [REF] pos_ux: intermediary account is required
    closes ingadhoc/sale#1206
    
    Signed-off-by: Franco Leyes <lef@adhoc.com.ar>

--- pos_ux/models/pos_config.py
+++ pos_ux/models/pos_config.py
@@ -1,4 +1,6 @@
 from odoo import fields, models
+from odoo.exceptions import UserError
+from odoo.tools.translate import _
 
 
 class PosConfig(models.Model):
@@ -13,3 +15,18 @@ class PosConfig(models.Model):
         default="on_demand",
     )
     block_invoice_download = fields.Boolean()
+
+    def open_ui(self):
+        for config in self:
+            invalid_payment_methods = config.payment_method_ids.filtered(
+                lambda method: not method.split_transactions and not method.receivable_account_id
+            )
+            if invalid_payment_methods:
+                payment_method_names = ", ".join(invalid_payment_methods.mapped("name"))
+                raise UserError(
+                    _(
+                        "No se puede completar la operación: falta definir cuenta intermediaria en los siguientes métodos de pago: %s"
+                    )
+                    % payment_method_names
+                )
+        return super().open_ui()
