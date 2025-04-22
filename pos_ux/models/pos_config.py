250d3ca128d8ae7e830c15be23cd2834ad443a6e
Matias Peralta <matias_peralta1@hotmail.com>
2025-04-22 14:14:45 -0300

    [REF] pos_ux: outstanding account into payment method or journal is required

--- pos_ux/models/pos_config.py
+++ pos_ux/models/pos_config.py
@@ -14,8 +14,19 @@ class PosConfig(models.Model):
     def open_ui(self):
         for config in self:
             invalid_payment_methods = config.payment_method_ids.filtered(
-            lambda method: not method.split_transactions and not method.receivable_account_id)
+                lambda method: not method.split_transactions and not method.receivable_account_id
+            )
             if invalid_payment_methods:
                 payment_method_names = ', '.join(invalid_payment_methods.mapped('name'))
                 raise UserError(_("No se puede completar la operación: falta definir cuenta intermediaria en los siguientes métodos de pago: %s") % payment_method_names)
+
+            payment_method_without_outstanding_account = config.payment_method_ids.filtered(
+                lambda method: method.journal_id.type == 'bank' and not method.outstanding_account_id and not any(
+                    line.payment_account_id for line in method.journal_id.inbound_payment_method_line_ids
+                )
+            )
+            if payment_method_without_outstanding_account:
+                payment_method_without_outstanding_account_names = ', '.join(payment_method_without_outstanding_account.mapped('name'))
+                raise UserError(_("No se puede completar la operación: falta definir la cuenta pendiente en los siguientes métodos de pago: %s") % payment_method_without_outstanding_account_names)
+
         return super().open_ui()
