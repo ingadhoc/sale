fddfa7574164e4cee3c28b101fbe848e7f6d6abb
Matias Peralta <matias_peralta1@hotmail.com>
2025-04-30 18:44:00 +0000

    [FIX] sale_order_type_ux: change narration or not from quotation to invoices
    closes ingadhoc/sale#1227
    
    X-original-commit: 7f09306f1ab03eb79da40d1bbdea2509f0e959e7
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>

--- sale_order_type_ux/models/sale_order.py
+++ sale_order_type_ux/models/sale_order.py
@@ -36,7 +36,7 @@ class SaleOrder(models.Model):
             res["company_id"] = company.id
             res["partner_bank_id"] = company.partner_id.bank_ids[:1].id
             # agregamos para que recompute term y cond si la nueva compañia los tiene por defecto
-            if "narration" in res:
+            if "narration" in res and not res["narration"]:
                 del res["narration"]
             so_fiscal_position = self.env["account.fiscal.position"].browse(res["fiscal_position_id"])
             if so_fiscal_position.company_id and so_fiscal_position.company_id != company:
