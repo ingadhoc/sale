ef84eaaf5367cbec905443259d6f475a53f291bd
Matias Peralta <matias_peralta1@hotmail.com>
2025-05-26 17:16:19 -0300

    [FIX] sale_exception_credit_limit: Validate credit_limit changes properly in write method for permission control

--- sale_exception_credit_limit/models/res_partner.py
+++ sale_exception_credit_limit/models/res_partner.py
@@ -15,13 +15,15 @@ class ResPartner(models.Model):
     def _compute_user_credit_config(self):
         self.user_credit_config = self.env.user.has_group("sale_exception_credit_limit.credit_config")
 
-    @api.constrains("credit_limit", "use_partner_credit_limit")
-    def check_credit_limit_group(self):
+    def write(self, vals):
         """Si esta constraint trae dolores de cabeza la podemos sacar ya que este "bache" de seguridad esta en muchos
-        lugares aún mas criticos. es un problema del ORM donde mucho se protege a nivel vista"""
-        if not self.env.user.has_group("sale_exception_credit_limit.credit_config") and any(
-            not x.parent_id or x.credit_limit != x.parent_id.credit_limit for x in self
-        ):
-            raise ValidationError(
-                "People without Credit limit Configuration Rights cannot modify credit limit parameters"
-            )
+        lugares aún mas criticos. Es un problema del ORM donde mucho se protege a nivel vista"""
+        if "credit_limit" in vals or "use_partner_credit_limit" in vals:
+            for record in self:
+                if not self.env.user.has_group("sale_exception_credit_limit.credit_config"):
+                    new_credit_limit = vals.get("credit_limit", record.credit_limit)
+                    if not record.parent_id or new_credit_limit != record.parent_id.credit_limit:
+                        raise ValidationError(
+                            "People without Credit limit Configuration Rights cannot modify credit limit parameters"
+                        )
+        return super().write(vals)
