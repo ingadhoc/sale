cad68db582e8dc16a161a386ba5f1254dd17bc96
Lara <lav@adhoc.com.ar>
2025-06-17 12:14:31 +0000

    [FIX] sale_gathering: hide fields is de so is not gathering
    closes ingadhoc/sale#1261
    
    Signed-off-by: Franco Leyes <lef@adhoc.com.ar>

--- sale_gathering_index/__manifest__.py
+++ sale_gathering_index/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Sale Gathering Index",
-    "version": "18.0.1.2.0",
+    "version": "18.0.1.3.0",
     "category": "Sales & Stock",
     "sequence": 14,
     "summary": "",
