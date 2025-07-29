38d4770319302ae8b7385b53d139866ba94f7650
Franco Leyes <lef@adhoc.com.ar>
2025-07-29 16:40:10 +0000

    [REF] sale_gathering_index: refactor price unit computation logic for gathering orders

--- sale_gathering_index/models/sale_order_line.py
+++ sale_gathering_index/models/sale_order_line.py
@@ -41,22 +41,13 @@ class SaleOrderLine(models.Model):
                 raise UserError(_("You can't modify the description."))
         return super().write(vals)
 
-    def _compute_price_unit(self):
-        super()._compute_price_unit()
-        for rec in self.filtered(
-            lambda x: x.order_id.is_gathering and x.order_id.state == "sale" and x.initial_qty_gathered == 0
-        ):
-            if not rec.product_uom or not rec.product_id:
-                price_unit = 0.0
-            else:
-                line = rec.with_company(rec.company_id)
-                price = line._get_display_price()
-                price_unit = line.product_id._get_tax_included_unit_price_from_price(
-                    price,
-                    product_taxes=line.product_id.taxes_id.filtered(lambda tax: tax.company_id == line.env.company),
-                    fiscal_position=line.order_id.fiscal_position_id,
-                )
-            rec.price_unit = price_unit / (rec.order_id.index + 1)
+    def _reset_price_unit(self):
+        super()._reset_price_unit()
+        if self.is_gathering and self.order_id.state == "sale" and self.initial_qty_gathered == 0:
+            self.update({
+                'price_unit':  self.price_unit / self.order_id.coef,
+                'technical_price_unit':  self.price_unit / self.order_id.coef,
+            })
 
     def _compute_name(self):
         super()._compute_name()
