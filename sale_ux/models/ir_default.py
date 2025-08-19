76ebb92357beb49355062958f1ac6c0f72e9b658
Virginia Bonservizi <96496796+vib-adhoc@users.noreply.github.com>
2025-08-19 16:52:05 -0300

    Update cleaner.yml
    closes ingadhoc/sale#1316
    
    Signed-off-by: Franco Leyes <lef@adhoc.com.ar>

---
+++ sale_ux/models/ir_default.py
@@ -0,0 +1,29 @@
+import json
+
+from odoo import models
+
+
+class IrDefault(models.Model):
+    _inherit = "ir.default"
+
+    def _get_model_defaults(self, model_name, condition=False):
+        res = super()._get_model_defaults(model_name, condition)
+        if model_name == "res.partner":
+            specific_property_pricelist = self.get_default_pricelist()
+            if specific_property_pricelist:
+                res["specific_property_product_pricelist"] = specific_property_pricelist
+        return res
+
+    def get_default_pricelist(self):
+        field = self.env["ir.model.fields"]._get("res.partner", "specific_property_product_pricelist")
+        default = self.env["ir.default"].search(
+            [
+                ("field_id", "=", field.id),
+                ("user_id", "=", self.env.context.get("uid", self.env.user.id)),
+                ("json_value", "!=", False),
+                ("company_id", "in", [self.env.company.id, False]),
+            ],
+            limit=1,
+            order="company_id desc",
+        )
+        return json.loads(default.json_value) if default else None
