805d593a03aa36088bb162caad2ec0ce7321b391
Martin Quinteros <maq@adhoc.com.ar>
2025-10-06 09:59:16 -0300

    [FIX] FIx in to_refund moves qty count

--- sale_stock_ux/models/stock_move.py
+++ sale_stock_ux/models/stock_move.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockMove(models.Model):
@@ -11,6 +11,30 @@ class StockMove(models.Model):
     sale_id = fields.Many2one(
         related="group_id.sale_id",
     )
+    is_exchange_move = fields.Boolean()
+
+    @api.model
+    def _prepare_merge_moves_distinct_fields(self):
+        fields = super()._prepare_merge_moves_distinct_fields()
+        fields.append("is_exchange_move")
+        return fields
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """La cantidad devuelta no deberia ser contada en nuevos movimientos de stock
+        que se creen a partir de una orden de venta.
+        Agregamos un HACK para que si esta instalado secondary unit sea recomputada en la creacion de
+        movimientos de stock.
+        TODO: Solo deberia impactar en movimientos de salida (uno o mas pasos)
+        """
+        for vals in vals_list:
+            if vals.get("sale_line_id"):
+                sale_line_qty_ret = self.env["sale.order.line"].browse(vals["sale_line_id"]).quantity_returned
+                vals["product_uom_qty"] -= sale_line_qty_ret
+                if "secondary_uom_qty" in vals:
+                    del vals["secondary_uom_qty"]
+
+        return super().create(vals_list)
 
     def _get_new_picking_values(self):
         """return create values for new picking that will be linked with group
