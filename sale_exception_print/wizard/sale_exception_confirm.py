0099a020538b11168803eb466fe5126cb2130ace
Franco Leyes <lef@adhoc.com.ar>
2025-10-14 17:20:33 -0300

    [MIG] sale_exception_print: Migration to 19.0

--- sale_exception_print/wizard/sale_exception_confirm.py
+++ sale_exception_print/wizard/sale_exception_confirm.py
@@ -9,7 +9,7 @@ class SaleExceptionConfirm(models.TransientModel):
     _inherit = "sale.exception.confirm"
 
     def action_confirm(self):
-        if self.ignore and self._context.get("print_exceptions", False):
+        if self.ignore and self.env.context.get("print_exceptions", False):
             self.related_model_id.ignore_exception_print = True
             # con esto limpiamos el ignore para que no super no ignore todo
             self.ignore = False
