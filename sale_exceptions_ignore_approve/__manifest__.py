99481df2fa4fc9f36cbcec48d2112b10aa196aee
Franco Leyes <lef@adhoc.com.ar>
2025-10-15 19:59:21 +0000

    [FIX] sale_exceptions_ignore_approve: delete changes for sale-workflow/3789
    X-original-commit: 85ea4ee46397249d240137b1ebb9730a34498556

--- sale_exceptions_ignore_approve/__manifest__.py
+++ sale_exceptions_ignore_approve/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Sale Exceptions Ingore Approve Directly",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "category": "Sale",
     "sequence": 14,
     "author": "ADHOC SA",
@@ -31,7 +31,6 @@
     ],
     "external_dependencies": {},
     "data": [
-        "wizards/sale_exception_confirm_views.xml",
         "views/sale_order_views.xml",
     ],
     "demo": [],
