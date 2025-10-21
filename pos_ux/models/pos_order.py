b89fe3c459e0b61d17d860a715abe143384597ee
Franco Leyes <lef@adhoc.com.ar>
2025-10-21 12:37:28 -0300

    [MIG] pos_ux: Migration to 19.0

--- pos_ux/models/pos_order.py
+++ pos_ux/models/pos_order.py
@@ -1,20 +1,13 @@
-from odoo import api, models
+from odoo import models
 
 
 class PosOrder(models.Model):
     _inherit = "pos.order"
 
-    @api.model
-    def _process_order(self, order, existing_order):
-        return super(PosOrder, self.with_context(from_process_order=True))._process_order(
-            order=order, existing_order=existing_order
-        )
-
-    def _generate_pos_order_invoice(self):
-        if self._context.get("from_process_order"):
-            return super(
-                PosOrder,
-                self.filtered(lambda x: not x.session_id.invoice_contingency).with_context(allow_no_partner=True),
-            )._generate_pos_order_invoice()
-        else:
-            return super(PosOrder, self.with_context(allow_no_partner=True))._generate_pos_order_invoice()
+    def _process_saved_order(self, draft):
+        if self.session_id.invoice_contingency and self.to_invoice:
+            self.to_invoice = False
+            result = super()._process_saved_order(draft)
+            self.to_invoice = True
+            return result
+        return super()._process_saved_order(draft)
