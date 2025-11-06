5e4925c24723c51856bbfca3c09bddd09a659c4b
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-11-06 18:04:57 +0000

    [MIG] sale_delivery_ux: Migration to 19.0

--- sale_delivery_ux/models/sale_order_line.py
+++ sale_delivery_ux/models/sale_order_line.py
@@ -12,7 +12,7 @@ class SaleOrderLine(models.Model):
         """
         we use this to skip change the qty for the delivery line with cost zero
         """
-        skip_validation = self._context.get("skip_validation", "")
+        skip_validation = self.env.context.get("skip_validation", "")
         if not skip_validation:
             return super()._get_protected_fields()
         fields = super()._get_protected_fields()
