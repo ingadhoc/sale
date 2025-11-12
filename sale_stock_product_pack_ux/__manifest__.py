e9d4f501b7e487d9cb920a307ac672c8420ac143
Franco Leyes <lef@adhoc.com.ar>
2025-11-12 20:48:45 +0000

    Cherry pick of 6b3405fcbf71ea6798aa38e008f2d3b6cb3e62b2 failed
    stdout:
    Auto-merging sale_stock_product_pack_ux/__manifest__.py
    CONFLICT (add/add): Merge conflict in sale_stock_product_pack_ux/__manifest__.py
    
    stderr:

--- sale_stock_product_pack_ux/__manifest__.py
+++ sale_stock_product_pack_ux/__manifest__.py
@@ -1,3 +1,4 @@
+<<<<<<< 877b11df7990357f7240262d4bd4f6e54a653655
 {
     "name": "Sale Stock Product Pack UX",
     "version": "18.0.1.0.0",
@@ -14,3 +15,22 @@
     "auto_install": True,
     "application": False,
 }
+||||||| 974af9b5f8e746744982d44cd4d0fb763b6b27e8
+=======
+{
+    "name": "Sale Stock Product Pack UX",
+    "version": "17.0.1.0.0",
+    "category": "Sales",
+    'summary': '',
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'license': 'AGPL-3',
+    "depends": [
+        "sale_stock_ux",
+        "sale_stock_product_pack",
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+>>>>>>> 6b3405fcbf71ea6798aa38e008f2d3b6cb3e62b2
