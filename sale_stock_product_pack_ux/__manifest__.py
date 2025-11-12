b2c04d3f448dd634b319323c1afac4904039ebc8
Franco Leyes <lef@adhoc.com.ar>
2025-11-12 20:48:45 +0000

    [ADD] sale_stock_product_pack_ux: add new module
    X-original-commit: 4ac66f7a49c378d59d117f2d8c1d4ad19c3cd2d2

---
+++ sale_stock_product_pack_ux/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Sale Stock Product Pack UX",
+    "version": "16.0.1.0.0",
+    "category": "Sales",
+    'summary': '',
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'license': 'AGPL-3',
+    "depends": [
+        "sale_stock_ux",
+        "sale_stock_product_pack",
+    ],
+    'installable': False,
+    'auto_install': False,
+    'application': False,
+}
