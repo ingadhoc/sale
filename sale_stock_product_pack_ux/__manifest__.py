02a28fbc55a5e38985da392b91aa7984dbc27437
Franco Leyes <lef@adhoc.com.ar>
2025-11-12 17:48:45 -0300

    [ADD] sale_stock_product_pack_ux: add new module
    closes ingadhoc/sale#1436
    
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>

---
+++ sale_stock_product_pack_ux/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Sale Stock Product Pack UX",
+    "version": "19.0.1.0.0",
+    "category": "Sales",
+    "summary": "",
+    "author": "ADHOC SA",
+    "website": "www.adhoc.com.ar",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_stock_ux",
+        "sale_stock_product_pack",
+    ],
+    "installable": True,
+    "auto_install": True,
+    "application": False,
+}
