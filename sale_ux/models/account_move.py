77dc27811e6ff6e58ddbd2a0962aaa1624726e55
“Victoria <hav@adhoc.inc>
2025-11-27 17:49:21 +0000

    [FIX]: sale_ux: do not propagate internal notes when not configured
    closes ingadhoc/sale#1483
    
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- sale_ux/models/account_move.py
+++ sale_ux/models/account_move.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountMove(models.Model):
@@ -12,6 +12,22 @@ class AccountMove(models.Model):
     sale_order_ids = fields.Many2many("sale.order", compute="_compute_sale_orders")
     # en la ui agregamos este que seria mejor a nivel performance
     has_sales = fields.Boolean(string="Has Sales?", compute="_compute_has_sales")
+
+    @api.depends("move_type", "partner_id", "partner_id.lang", "company_id")
+    def _compute_narration(self):
+        """Override para respetar el parámetro propagate_note desde sale orders"""
+        propagate_note = self.env["ir.config_parameter"].sudo().get_param("sale.propagate_note") == "True"
+
+        # Si propagate_note está activado y la factura viene de una SO, no tocar la narración
+        if propagate_note:
+            invoices_from_so = self.filtered(lambda m: m.invoice_origin)
+            invoices_to_compute = self - invoices_from_so
+        else:
+            invoices_to_compute = self
+
+        # Aplicar la lógica original solo a facturas que no vienen de SO o cuando propagate_note=False
+        if invoices_to_compute:
+            super(AccountMove, invoices_to_compute)._compute_narration()
 
     def _compute_sale_orders(self):
         for rec in self:
