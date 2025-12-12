c600c767c450c562e4ad3fa6d190ca9f982dfca7
Franco Leyes <lef@adhoc.com.ar>
2025-12-12 17:27:31 +0000

    [ADD] sale_stock_product_pack_automation: add new module
    X-original-commit: ac06bb4cc9f894bd50e390ee7aa3c7ecd03f66bc

---
+++ sale_stock_product_pack_automation/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Sale Stock Product Pack Automation",
+    "version": "18.0.1.0.0",
+    "category": "Sales",
+    "summary": "",
+    "author": "ADHOC SA",
+    "website": "www.adhoc.inc",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_stock_product_pack_ux",
+        "sale_order_type_automation",
+    ],
+    "installable": False,
+    "auto_install": True,
+    "application": False,
+}
