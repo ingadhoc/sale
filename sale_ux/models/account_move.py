b057b087c16957dae9d913e1fbe304e27e60e2bb
Rocío Vega <rov@adhoc.com.ar>
2025-12-16 11:58:45 +0000

    [FIX] sale_ux: fix downpayment price unit calculation for currency changes
    closes ingadhoc/sale#1509
    
    X-original-commit: 9f9f841ffcc3e275f074d3aa86789de605c3681c
    Signed-off-by: lav-adhoc <lav@adhoc.com.ar>
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>

--- sale_ux/models/account_move.py
+++ sale_ux/models/account_move.py
@@ -31,10 +31,13 @@ class AccountMove(models.Model):
         for downpayment_line in downpayment_lines:
             # When change currency in downpayment
             if self.currency_id != downpayment_line.currency_id:
-                downpayment_line.price_unit = self.currency_id._convert(
-                    downpayment_line.price_unit,
-                    downpayment_line.currency_id,
-                    self.company_id,
-                    self.invoice_date or fields.Date.today(),
+                downpayment_invoice_line = self.invoice_line_ids.filtered(
+                    lambda l: l.is_downpayment and l.sale_line_ids.ids == downpayment_line.ids
                 )
+                downpayment_invoice_line.ensure_one()
+                price_unit = downpayment_invoice_line.price_unit
+                converted_price_unit = self.currency_id._convert(
+                    price_unit, downpayment_line.currency_id, self.company_id, self.invoice_date or fields.Date.today()
+                )
+                downpayment_line.price_unit = converted_price_unit
         return res
