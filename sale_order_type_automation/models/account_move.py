9babcb59bb02fbb5d9713ab7bbe790342f68e64e
Rocío Vega <rov@adhoc.com.ar>
2026-01-12 19:00:32 +0000

    [FIX] sale_order_type_automation: correctly set sale_type_id when running automation of invoicing instead of using default sale type id that is not correct when the sale already has one defined
    X-original-commit: d638973fd6558800bf5e6d9f97eb4ad74b976473

--- sale_order_type_automation/models/account_move.py
+++ sale_order_type_automation/models/account_move.py
@@ -20,6 +20,7 @@ class AccountMove(models.Model):
             "currency_id": invoice.currency_id.id,
         }
 
+<<<<<<< 92a268dbfba4f2b4b51054108cc07533e6f08bcc
     def _register_payment_invoice(self, invoice, payment_journal):
         payment = self.env["account.payment"].create(self._prepare_dict_account_payment(invoice, payment_journal))
         payment.action_post()
@@ -49,3 +50,25 @@ class AccountMove(models.Model):
                 message = "Could not automatically create and validate payment. Error: %s" % error
                 invoice.message_post(body=message)
         return res
+||||||| 3da0cf614caff1162b31c37b0692599831fecc87
+    def _compute_company_id(self):
+        super()._compute_company_id()
+        # If company_id is empty after super (because _accessible_branches returned empty),
+        # assign the journal's company directly. This happens when in sudo mode and the user
+        # has no access to any company
+        for move in self.filtered(
+            lambda m: not m.company_id and m.sale_type_id.journal_id and m.sale_type_id.invoicing_atomation != "none"
+        ):
+            move.company_id = move.sale_type_id.journal_id.company_id
+=======
+    def _compute_company_id(self):
+        super()._compute_company_id()
+        # If company_id is empty after super (because _accessible_branches returned empty),
+        # assign the journal's company directly. This happens when in sudo mode and the user
+        # has no access to any company
+        for move in self.filtered(lambda m: not m.company_id):
+            sale_type_id = self.env["sale.order.type"].browse(self.env.context.get("sale_type_id")) or move.sale_type_id  # noqa
+
+            if sale_type_id.journal_id and sale_type_id.invoicing_atomation != "none":
+                move.company_id = sale_type_id.journal_id.company_id
+>>>>>>> f2dd1eaaab9e0a0fc8b2b6ed059af7644c879fa6
