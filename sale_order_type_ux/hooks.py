1666c42c87972d8a3ce87c94094bd225ff063863
Rocío Vega <rov@adhoc.com.ar>
2026-02-19 12:13:31 +0000

    [FIX] _revert_method: ensure origin method is set only if it exists
    X-original-commit: 979d25213b6b02f0fc6eb5f9e9f4ce4975694774

--- sale_order_type_ux/hooks.py
+++ sale_order_type_ux/hooks.py
@@ -14,3 +14,33 @@ def post_init_hook(env):
     """,
         (default_sale_order_type.id,),
     )
+<<<<<<< 43252cd02aa1fe6c706a976ef150f598a8e356ba
+||||||| 966dc41c0e7846e295354deaf879bef06c9c9a6b
+
+
+def _revert_method(cls, name):
+    """Revert the original method called ``name`` in the given class.
+    See :meth:`~._patch_method`.
+    """
+    method = getattr(cls, name)
+    setattr(cls, name, method.origin)
+
+
+def uninstall_hook(env):
+    _revert_method(AccountMove, "_compute_journal_id")
+=======
+
+
+def _revert_method(cls, name):
+    """Revert the original method called ``name`` in the given class.
+    See :meth:`~._patch_method`.
+    """
+    method = getattr(cls, name)
+    origin = getattr(method, "origin", None)
+    if origin:
+        setattr(cls, name, origin)
+
+
+def uninstall_hook(env):
+    _revert_method(AccountMove, "_compute_journal_id")
+>>>>>>> 917efa734e1881011f99ca997400d4012b25e556
