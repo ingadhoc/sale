130e96b632a1a57ac1a4f78adcb944c328323551
Rocío Vega <rov@adhoc.com.ar>
2026-02-25 09:24:36 -0300

    [FIX] sale_ux: ensure image_sale_order computation only runs if field exists

--- sale_ux/migrations/18.0.1.18.0/post-migration.py
+++ sale_ux/migrations/18.0.1.18.0/post-migration.py
@@ -7,8 +7,9 @@ _logger = logging.getLogger(__name__)
 
 def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    product_ids = env["product.template"].search([("image_128", "!=", False), ("image_sale_order", "=", False)])
-    for i in range(0, len(product_ids), 100):
-        product_chunk = product_ids[i : i + 100]
-        product_chunk._compute_image_sale_order()
-        _logger.info("Recomputed image_sale_order for %d products", i + 100)
+    if "image_sale_order" in env["product.template"]._fields:
+        product_ids = env["product.template"].search([("image_128", "!=", False), ("image_sale_order", "=", False)])
+        for i in range(0, len(product_ids), 100):
+            product_chunk = product_ids[i : i + 100]
+            product_chunk._compute_image_sale_order()
+            _logger.info("Recomputed image_sale_order for %d products", i + 100)
