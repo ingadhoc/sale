3f6c3eae4285e04d95e735f2658e56f7670953a2
Guido Galetto <gal@adhoc.inc>
2026-03-16 19:24:39 +0000

    [FIX] sale_order_type_project: recalculates project_id field based on type
    X-original-commit: 52e400ddfe6b8c04cf0d4052f14bd68cad59848b

---
+++ sale_order_type_project/models/sale_order.py
@@ -0,0 +1,25 @@
+<<<<<<< HEAD
+||||||| MERGE BASE
+=======
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    project_id = fields.Many2one(
+        compute="_compute_project_id",
+        store=True,
+        readonly=False,
+        precompute=True,
+    )
+
+    @api.depends("type_id")
+    def _compute_project_id(self):
+        for order in self:
+            if order.type_id and order.type_id.project_id:
+                order.project_id = order.type_id.project_id
+
+>>>>>>> FORWARD PORTED
