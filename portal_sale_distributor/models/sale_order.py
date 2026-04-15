187fcded87d657fd4e3f5c03712d1535ce3ff0bc
Guido Galetto <gal@adhoc.inc>
2026-04-15 12:42:29 +0000

    [IMP] portal_sale_distributor: make validity_date readonly for distributor portal users

--- portal_sale_distributor/models/sale_order.py
+++ portal_sale_distributor/models/sale_order.py
@@ -11,7 +11,7 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     activity_date_deadline = fields.Date(
-        groups="base.group_user," "portal_sale_distributor.group_portal_backend_distributor"
+        groups="base.group_user,portal_sale_distributor.group_portal_backend_distributor"
     )
 
     def action_confirm_distributor(self):
@@ -60,6 +60,7 @@ class SaleOrder(models.Model):
                 + arch.xpath("//field[@name='discount3']")
                 + arch.xpath("//field[@name='tax_id']")
                 + arch.xpath("//field[@name='validity_days']")
+                + arch.xpath("//field[@name='validity_date']")
             )
             for node in readonly_fields:
                 node.set("readonly", "1")
