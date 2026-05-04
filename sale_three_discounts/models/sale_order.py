5e53b3dca698f7663414e3a37bbd41a747ce0400
Guido Galetto <gal@adhoc.inc>
2026-05-04 17:24:02 +0000

    [FIX] sale_three_discounts: preserve manual discounts when confirming order
    X-original-commit: f9f1260b4283dfb88d30658fca73ec32f3e5b1cd

---
+++ sale_three_discounts/models/sale_order.py
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+||||||| MERGE BASE
+=======
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def action_confirm(self):
+        return super(SaleOrder, self.with_context(confirming_order=True)).action_confirm()
+
+    def _recompute_prices(self):
+        super(SaleOrder, self.with_context(recompute_prices=True))._recompute_prices()
+
+>>>>>>> FORWARD PORTED
