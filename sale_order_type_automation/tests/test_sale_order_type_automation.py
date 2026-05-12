6eaea6170485e9408503f67dcbba4d992ae3c5a4
Franco Leyes <lef@adhoc.com.ar>
2026-05-12 20:46:26 +0000

    [IMP] sale_order_type_automation: improve automation coverage

--- sale_order_type_automation/tests/test_sale_order_type_automation.py
+++ sale_order_type_automation/tests/test_sale_order_type_automation.py
@@ -2,49 +2,117 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-import odoo.tests.common as common
+from odoo import Command
+from odoo.exceptions import ValidationError
+from odoo.tests import TransactionCase, tagged
 
 
-class TestSaleOrderTypeAutomation(common.TransactionCase):
-    def setUp(self):
-        # TODO mejorar los tests
-        super().setUp()
-        self.sale_order_model = self.env["sale.order"]
-        self.sale_order_type = self.env.ref("sale_order_type.normal_sale_type")
-        self.sequence = self.env["ir.sequence"].create(
+@tagged("post_install", "-at_install")
+class TestSaleOrderTypeAutomation(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.partner = cls.env.ref("base.res_partner_1")
+        cls.sale_type = cls.env.ref("sale_order_type.normal_sale_type")
+        cls.product = cls.env["product.product"].create(
             {
-                "name": "Test Sales Order 2",
-                "code": "sale.order",
-                "prefix": "TSO2",
-                "padding": 3,
-                "company_id": self.sale_order_type.warehouse_id.company_id.id,
+                "name": "Sale Order Type Automation Product",
+                "type": "consu",
+                "invoice_policy": "order",
+                "list_price": 100.0,
             }
         )
-        self.partner = self.env.ref("base.res_partner_1")
-        self.product = self.env.ref("product.product_product_4")
-        self.payment_journal = self.env["account.journal"].search([("type", "=", "cash")], limit=1)
-        self.sale_type = self.sale_order_type.write(
-            {
-                # 'validate_automatically_picking': True,
-                # 'validate_automatically_invoice': True,
-                # 'validate_automatically_payment': True,
-                # 'payment_journal_id': self.payment_journal.id,
-                "sequence_id": self.sequence.id,
-            }
+        cls.payment_journal = cls.env["account.journal"].search(
+            [
+                ("type", "in", ["cash", "bank"]),
+                ("inbound_payment_method_line_ids.code", "=", "manual"),
+                ("company_id", "=", cls.sale_type.invoice_company_id.id),
+            ],
+            limit=1,
         )
 
-    def test_sale_order_confirm(self):
-        sale_line_dict = {
-            "product_id": self.product.id,
-            "name": self.product.name,
-            "product_uom_qty": 1.0,
-            "product_uom_id": self.product.uom_id.id,
-            "price_unit": self.product.list_price,
-        }
+    def _create_order(self, **extra_vals):
         vals = {
             "partner_id": self.partner.id,
-            "type_id": self.sale_order_type.id,
-            "order_line": [(0, 0, sale_line_dict)],
+            "type_id": self.sale_type.id,
+            "order_line": [
+                Command.create(
+                    {
+                        "product_id": self.product.id,
+                        "product_uom_qty": 1.0,
+                        "price_unit": 100.0,
+                    }
+                )
+            ],
         }
-        sale_order = self.sale_order_model.create(vals)
-        sale_order.action_confirm()
+        vals.update(extra_vals)
+        return self.env["sale.order"].with_context(ignore_exception=True).create(vals)
+
+    def test_action_confirm_creates_invoice_when_configured(self):
+        self.sale_type.write(
+            {
+                "invoicing_atomation": "create_invoice",
+                "picking_atomation": "none",
+                "set_done_on_confirmation": False,
+                "invoice_validate_domain": False,
+            }
+        )
+        order = self._create_order()
+
+        order.with_context(ignore_exception=True).action_confirm()
+
+        self.assertTrue(order.invoice_ids)
+        self.assertEqual(set(order.invoice_ids.mapped("state")), {"draft"})
+
+    def test_action_confirm_posts_invoice_when_validate_mode(self):
+        self.sale_type.write(
+            {
+                "invoicing_atomation": "validate_invoice",
+                "picking_atomation": "none",
+                "invoice_validate_domain": False,
+            }
+        )
+        order = self._create_order()
+
+        order.with_context(ignore_exception=True).action_confirm()
+
+        self.assertTrue(order.invoice_ids)
+        self.assertEqual(set(order.invoice_ids.mapped("state")), {"posted"})
+
+    def test_action_confirm_sets_order_done_when_configured(self):
+        self.sale_type.write(
+            {
+                "invoicing_atomation": "none",
+                "picking_atomation": "none",
+                "set_done_on_confirmation": True,
+            }
+        )
+        order = self._create_order()
+
+        order.with_context(ignore_exception=True).action_confirm()
+
+        self.assertEqual(order.state, "sale")
+        self.assertTrue(order.locked)
+
+    def test_validate_payment_automation_requires_payment_journal(self):
+        with self.assertRaises(ValidationError):
+            self.sale_type.write(
+                {
+                    "payment_atomation": "validate_payment",
+                    "payment_journal_id": False,
+                }
+            )
+
+    def test_validate_payment_automation_accepts_valid_payment_journal(self):
+        if not self.payment_journal:
+            self.skipTest("No manual payment journal available for current company")
+
+        self.sale_type.write(
+            {
+                "payment_journal_id": self.payment_journal.id,
+                "payment_atomation": "validate_payment",
+            }
+        )
+
+        self.assertEqual(self.sale_type.payment_atomation, "validate_payment")
+        self.assertEqual(self.sale_type.payment_journal_id, self.payment_journal)
