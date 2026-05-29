9d5ed3b4aef63f423dc7d17b3322a27216a6b100
Camila Vives <cav@adhoc.com.ar>
2026-05-29 20:18:27 +0000

    [FIX] sale_ux: fix group manipulation in tests (use write instead of -=)
    Part-of: ingadhoc/sale#1687
    Related: ingadhoc/account-financial-tools#964
    Signed-off-by: Filoquin adhoc <maq@adhoc.com.ar>

--- sale_ux/tests/test_fiscal_position.py
+++ sale_ux/tests/test_fiscal_position.py
@@ -6,8 +6,6 @@ class TestSaleOrderFiscalPosition(TransactionCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.delivery_group = cls.env.ref("account.group_delivery_invoice_address")
-        cls.env.user.groups_id -= cls.delivery_group
-
         cls.fiscal_position = cls.env["account.fiscal.position"].create({"name": "Test FP"})
         cls.partner = cls.env["res.partner"].create(
             {
@@ -15,6 +13,11 @@ class TestSaleOrderFiscalPosition(TransactionCase):
                 "property_account_position_id": cls.fiscal_position.id,
             }
         )
+
+    def setUp(self):
+        super().setUp()
+        # Ensure user doesn't have the delivery group so our override is executed
+        self.env.user.write({"group_ids": [(3, self.delivery_group.id)]})
 
     def test_fiscal_position_computed_from_partner(self):
         """Without group_delivery_invoice_address, fiscal position is set from partner."""
@@ -32,7 +35,7 @@ class TestSaleOrderFiscalPosition(TransactionCase):
 
     def test_fiscal_position_fallback_to_super_with_group(self):
         """With group_delivery_invoice_address, the standard Odoo compute is used."""
-        self.env.user.groups_id |= self.delivery_group
+        self.env.user.write({"group_ids": [(4, self.delivery_group.id)]})
         order = self.env["sale.order"].create({"partner_id": self.partner.id})
         # super() also resolves to partner's property_account_position_id in this case
         self.assertEqual(order.fiscal_position_id, self.fiscal_position)
