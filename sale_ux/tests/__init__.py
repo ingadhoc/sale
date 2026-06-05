45878367c0bb87f7fc524316c25165b219e926b9
Franco Leyes <lef@adhoc.com.ar>
2026-06-05 19:51:48 +0000

    [FIX] sale_ux: add tests and handle missing product_id in advance payment wizard
    closes ingadhoc/sale#1673
    
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>

--- sale_ux/tests/__init__.py
+++ sale_ux/tests/__init__.py
@@ -1,3 +1,7 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
 from . import test_crm_team
 from . import test_account_move
 from . import test_partner_product_config
@@ -5,3 +9,4 @@ from . import test_sale_order
 from . import test_sale_order_line
 from . import test_settings
 from . import test_wizards
+from . import test_sale_order_unlink
