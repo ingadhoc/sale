46d6a91e85e2f825f74eac67f5f3a1401d730f1e
Juan Ignacio Carreras <jc@adhoc.com.ar>
2026-07-07 23:27:46 +0000

    Cherry pick of 836ec104edef0e61d9964120a3aec8be4738aa37 failed
    stdout:
    Auto-merging sale_stock_ux/models/sale_order_line.py
    CONFLICT (content): Merge conflict in sale_stock_ux/models/sale_order_line.py
    
    stderr:

--- sale_stock_ux/tests/__init__.py
+++ sale_stock_ux/tests/__init__.py
@@ -1,1 +1,8 @@
 from . import test_action_cancel
+<<<<<<< 45158847ecfb019e152733cc2218df6e704a0b9d
+||||||| 53dc870e73a970e657c6909b8cf657b3ce51cf6d
+from . import test_return_of_return
+from . import test_cancel_remaining_contraentrega
+=======
+from . import test_return_of_return
+>>>>>>> bc26ec8c87bd1d58c86b40c36a7dfad48808da23
