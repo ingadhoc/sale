f40285cd9f028d6100b71489fedd57c2b310882d
roboadhoc <roboadhoc@adhoc.com.ar>
2026-09-04 22:02:55 +0000

    [BOT] Bump version: sale_order_type_automation 19.0.2.4.0
    Merged: ingadhoc/sale#1793

--- sale_order_type_automation/__manifest__.py
+++ sale_order_type_automation/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Sale Order Type Automation",
-    "version": "19.0.2.3.0",
+    "version": "19.0.2.4.0",
     "author": "ADHOC SA",
     "website": "www.adhoc.com.ar",
     "license": "AGPL-3",
