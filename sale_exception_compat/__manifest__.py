cd1ac2da921fef701b6b80cd6da9b18768077d10
roboadhoc <roboadhoc@adhoc.com.ar>
2026-09-21 17:58:52 +0000

    [BOT] Bump version: sale_exception_compat 18.0.1.1.0
    Merged: ingadhoc/sale#1864

--- sale_exception_compat/__manifest__.py
+++ sale_exception_compat/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Sale Exception Compat",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "category": "Sale",
     "author": "ADHOC SA",
     "website": "www.adhoc.com.ar",
